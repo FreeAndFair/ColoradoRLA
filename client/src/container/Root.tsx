586dbe3180c9da112adfbd93355deb2e32abad6a
Joe Ranweiler <joe@lemma.co>
2017-07-18 11:51:48 -0700

    Add plumbing for easy-to-manage route definitions.

--- client/src/container/Root.tsx
+++ client/src/container/Root.tsx
@@ -3,6 +3,7 @@ import { Provider, Store } from 'react-redux';
 import {
     BrowserRouter as Router,
     Route,
+    Switch,
 } from 'react-router-dom';
 
 import App from './App';
@@ -12,6 +13,17 @@ export interface RootProps {
     store: Store<any>;
 }
 
+type RouteDef = [string, React.ComponentClass];
+
+const makeRoute = (def: RouteDef) => {
+    const [path, Component] = def;
+    return <Route exact key={ path } path={ path } component={ Component } />;
+};
+
+const routes: RouteDef[] = [
+    ['/', App],
+];
+
 export class Root extends React.Component<RootProps, void> {
     public render() {
         const { store } = this.props;
@@ -19,7 +31,9 @@ export class Root extends React.Component<RootProps, void> {
         return (
             <Provider store={store}>
                 <Router>
-                    <Route path='/' component={ App } />
+                    <Switch>
+                        { routes.map(makeRoute) }
+                    </Switch>
                 </Router>
             </Provider>
         );
