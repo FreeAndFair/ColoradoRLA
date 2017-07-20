5080cd79366425c506bf249dac39d6c0c5a5f860
Joe Ranweiler <joe@lemma.co>
2017-07-20 10:00:27 -0700

    Add login form fields and submit button

--- client/src/component/LoginForm.tsx
+++ client/src/component/LoginForm.tsx
@@ -3,6 +3,18 @@ import * as React from 'react';
 
 export default class LoginForm extends React.Component<any, any> {
     public render() {
-        return <div>Login form</div>;
+        return (
+            <div>
+                <label className='pt-label'>
+                    Email
+                    <input className='pt-input' type='text' />
+                </label>
+                <label className='pt-label'>
+                    Password
+                    <input className='pt-input' type='password' />
+                </label>
+                <button className='pt-primary'>Submit</button>
+            </div>
+        );
     }
 }
