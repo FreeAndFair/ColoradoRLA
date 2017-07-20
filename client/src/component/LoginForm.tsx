e82acf79e3b4ed11f10f17371806e8008b08a148
Joe Ranweiler <joe@lemma.co>
2017-07-20 09:52:18 -0700

    Scaffold login page component structure.

---
+++ client/src/component/LoginForm.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+
+
+export default class LoginForm extends React.Component<any, any> {
+    public render() {
+        return <div>Login form</div>;
+    }
+}
--- client/src/container/Login.tsx
+++ client/src/container/Login.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
 
+import LoginPage from '../component/LoginPage';
+
 
 class Login extends React.Component<any, any> {
     public render() {
-        return <div>Login</div>;
+        return <LoginPage />;
     }
 }
 
