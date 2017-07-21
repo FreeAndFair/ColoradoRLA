81cbf32de9265fb9134b2b3a73244da11fc53664
Joe Ranweiler <joe@lemma.co>
2017-07-21 14:25:22 -0700

    Rename exported container classes.

--- client/src/container/Report.tsx
+++ client/src/container/Report.tsx
@@ -4,12 +4,12 @@ import { connect } from 'react-redux';
 import Nav from '../component/Nav';
 
 
-class Report extends React.Component<any, any> {
+class ReportContainer extends React.Component<any, any> {
     public render() {
         return (
             <div>
                 <Nav />
-                <div>Report</div>
+                <div>ReportContainer</div>
             </div>
         );
     }
@@ -19,4 +19,4 @@ const mapStateToProps = (state: any) => ({});
 
 const mapDispatchToProps = (dispatch: any) => ({});
 
-export default connect(mapStateToProps, mapDispatchToProps)(Report);
+export default connect(mapStateToProps, mapDispatchToProps)(ReportContainer);
--- client/src/container/Upload.tsx
+++ client/src/container/Upload.tsx
@@ -4,12 +4,12 @@ import { connect } from 'react-redux';
 import Nav from '../component/Nav';
 
 
-class Upload extends React.Component<any, any> {
+class UploadContainer extends React.Component<any, any> {
     public render() {
         return (
             <div>
                 <Nav />
-                <div>Upload</div>
+                <div>UploadContainer</div>
             </div>
         );
     }
@@ -19,4 +19,4 @@ const mapStateToProps = (state: any) => ({});
 
 const mapDispatchToProps = (dispatch: any) => ({});
 
-export default connect(mapStateToProps, mapDispatchToProps)(Upload);
+export default connect(mapStateToProps, mapDispatchToProps)(UploadContainer);
