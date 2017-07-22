4dde9e9c3a0b85b3557cf0f77cfd58391ce6361c
Joe Ranweiler <joe@lemma.co>
2017-07-22 16:31:33 -0700

    Make login form a controlled component.

--- client/src/component/LoginForm.test.tsx
+++ client/src/component/LoginForm.test.tsx
@@ -10,16 +10,15 @@ import LoginForm from './LoginForm';
 test('LoginForm', s => {
 
     s.test('initial state', t => {
-        t.plan(4);
+        t.plan(3);
 
         const c = shallow(<LoginForm />);
-        t.ok(!c.state('valid'), 'initial state is invalid');
 
         const emailField = c.find('input.email').first();
-        t.equal(emailField.text(), '', 'the email field is empty');
+        t.equal(emailField.prop('value'), '', 'the email field is empty');
 
         const passwordField = c.find('input.password').first();
-        t.equal(passwordField.text(), '', 'the password field is empty');
+        t.equal(passwordField.prop('value'), '', 'the password field is empty');
 
         const submitButton = c.find('button.submit').first();
         t.ok(submitButton.prop('disabled'), 'submit button is disabled');
--- client/src/component/LoginForm.tsx
+++ client/src/component/LoginForm.tsx
@@ -1,30 +1,56 @@
 import * as React from 'react';
 
 
+interface FormFields {
+    email: string;
+    password: string;
+}
+
+function isFormValid(form: FormFields): boolean {
+    return false;
+}
+
 export default class LoginForm extends React.Component<any, any> {
     constructor() {
         super();
 
-        this.state = { valid: false };
+        this.state = {
+            form: {
+                email: '',
+                password: '',
+            },
+        };
     }
 
     public render() {
-        const disabled = !this.state.valid;
+        const { form } = this.state;
+        const disabled = !isFormValid(form);
 
         return (
             <div>
                 <label className='pt-label'>
                     Email
-                    <input className='pt-input email' type='text' />
+                    <input className='pt-input email'
+                           type='text'
+                           value={ form.email } />
                 </label>
                 <label className='pt-label'>
                     Password
-                    <input className='pt-input password' type='password' />
+                    <input className='pt-input password'
+                           type='password'
+                           value={ form.password } />
                 </label>
-                <button disabled={ disabled } className='pt-primary submit'>
+                <button
+                    disabled={ disabled }
+                    className='pt-primary submit'
+                    onClick={ this.buttonClick }>
                     Submit
                 </button>
             </div>
         );
+    }
+
+    private buttonClick(e: React.MouseEvent<HTMLButtonElement>) {
+        return;
     }
 }
