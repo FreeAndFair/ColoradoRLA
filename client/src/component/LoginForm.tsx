1f48eac006a9bf5f87a6dfaadb94d660fcac9cff
Joe Ranweiler <joe@lemma.co>
2017-07-21 21:16:11 -0700

    Add login form initial local state.

--- client/src/component/LoginForm.tsx
+++ client/src/component/LoginForm.tsx
@@ -2,18 +2,28 @@ import * as React from 'react';
 
 
 export default class LoginForm extends React.Component<any, any> {
+    constructor() {
+        super();
+
+        this.state = { valid: false };
+    }
+
     public render() {
+        const disabled = !this.state.valid;
+
         return (
             <div>
                 <label className='pt-label'>
                     Email
-                    <input className='pt-input' type='text' />
+                    <input className='pt-input email' type='text' />
                 </label>
                 <label className='pt-label'>
                     Password
-                    <input className='pt-input' type='password' />
+                    <input className='pt-input password' type='password' />
                 </label>
-                <button className='pt-primary'>Submit</button>
+                <button disabled={ disabled } className='pt-primary submit'>
+                    Submit
+                </button>
             </div>
         );
     }
