01d3abcb8ee05442912b6fad3819c8271092d7c4
Joe Ranweiler <joe@lemma.co>
2017-07-22 17:05:24 -0700

    Add login form validation.

--- client/src/component/LoginForm.test.tsx
+++ client/src/component/LoginForm.test.tsx
@@ -8,7 +8,6 @@ import LoginForm from './LoginForm';
 
 
 test('LoginForm', s => {
-
     s.test('initial state', t => {
         t.plan(3);
 
@@ -24,4 +23,68 @@ test('LoginForm', s => {
         t.ok(submitButton.prop('disabled'), 'submit button is disabled');
     });
 
+    s.test('with email only', t => {
+        t.plan(3);
+
+        const c = shallow(<LoginForm />);
+
+        const EMAIL = 'user@example.com';
+        const nextState = c.state();
+        nextState.form.email = EMAIL;
+        c.setState(nextState);
+
+        const emailField = c.find('input.email').first();
+        t.equal(emailField.prop('value'), EMAIL, 'the email field is non-empty');
+
+        const passwordField = c.find('input.password').first();
+        t.equal(passwordField.prop('value'), '', 'the password field is empty');
+
+        const submitButton = c.find('button.submit').first();
+        t.ok(submitButton.prop('disabled'), 'submit button is disabled');
+    });
+
+    s.test('with password only', t => {
+        t.plan(3);
+
+        const c = shallow(<LoginForm />);
+
+        const PASSWORD = 'hunter2';
+        const nextState = c.state();
+        nextState.form.password = PASSWORD;
+        c.setState(nextState);
+
+        const emailField = c.find('input.email').first();
+        t.equal(emailField.prop('value'), '',
+                'the email field is empty');
+
+        const passwordField = c.find('input.password').first();
+        t.equal(passwordField.prop('value'), PASSWORD,
+                'the password field is non-empty');
+
+        const submitButton = c.find('button.submit').first();
+        t.ok(submitButton.prop('disabled'), 'submit button is disabled');
+    });
+
+    s.test('with email and password', t => {
+        t.plan(3);
+
+        const c = shallow(<LoginForm />);
+
+        const EMAIL = 'user@example.com';
+        const PASSWORD = 'hunter2';
+        const nextState = c.state();
+        nextState.form = { email: EMAIL, password: PASSWORD };
+        c.setState(nextState);
+
+        const emailField = c.find('input.email').first();
+        t.equal(emailField.prop('value'), EMAIL,
+                'the email field is non-empty');
+
+        const passwordField = c.find('input.password').first();
+        t.equal(passwordField.prop('value'), PASSWORD,
+                'the password field is non-empty');
+
+        const submitButton = c.find('button.submit').first();
+        t.ok(!submitButton.prop('disabled'), 'submit button is enabled');
+    });
 });
