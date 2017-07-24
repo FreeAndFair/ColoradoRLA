6e5c1bd7d0f2a483c02cc56fbb46dcab72e257f4
Joe Ranweiler <joe@lemma.co>
2017-07-24 11:26:14 -0700

    Add SoS nav menu.

---
+++ client/src/component/sos/index.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+import SoSNav from './Nav';
+
+
+export default class SoSRootPage extends React.Component<any, any> {
+    public render() {
+        return (
+            <div>
+                <SoSNav />
+            </div>
+        );
+    }
+}
