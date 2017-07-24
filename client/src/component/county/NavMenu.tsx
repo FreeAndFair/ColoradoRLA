15ec3fda7b4e128a1b5e08ceeb60c2143efc0327
Joe Ranweiler <joe@lemma.co>
2017-07-24 12:40:42 -0700

    Add County nav menu.

---
+++ client/src/component/county/NavMenu.tsx
@@ -0,0 +1,44 @@
+import * as React from 'react';
+
+import { Link } from 'react-router-dom';
+
+
+interface MenuItemDef {
+    icon: string;
+    path: string;
+    text: string;
+}
+
+const MenuItem = (def: MenuItemDef): any => {
+    const className = `pt-menu-item ${def.icon}`;
+
+    return (
+        <Link to={ def.path }>
+            <li>
+                <span className={ className }>
+                    { def.text }
+                </span>
+            </li>
+        </Link>
+    );
+};
+
+export default class CountyNavMenu extends React.Component<any, any> {
+    public render() {
+        return (
+            <ul className='pt-menu pt-election-1'>
+                <MenuItem
+                    text='Home'
+                    path='/county'
+                    icon='pt-icon-home'
+                />
+                <li className='pt-menu-divider' />
+                <MenuItem
+                    text='Audit'
+                    path='/county/audit'
+                    icon='pt-icon-eye-open'
+                />
+            </ul>
+        );
+    }
+}
