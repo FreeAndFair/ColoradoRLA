9070b24a21a724dff00c7666cf98146d06129a37
Joe Ranweiler <joe@lemma.co>
2017-07-25 16:41:22 -0700

    Draft county audit wizard, start.

--- client/src/component/county/audit/index.tsx
+++ client/src/component/county/audit/index.tsx
@@ -3,11 +3,29 @@ import * as React from 'react';
 import CountyNav from '../Nav';
 
 
+const WizardStart = () => (
+    <div>
+        <div>Acme County Audit</div>
+        <div className='pt-card'>
+            <div>Acme County General Election Audit</div>
+            <div>Election date: 11/21/2017</div>
+            <div>County & State Ballot Contests</div>
+            <button className='pt-button'>Start My Audit</button>
+        </div>
+    </div>
+);
+
+const CountyAuditWizard = () => (
+    <div>
+        <WizardStart />
+    </div>
+);
+
 const CountyAuditPage = () => {
     return (
         <div>
             <CountyNav />
-            <div>Audit root.</div>
+            <CountyAuditWizard />
         </div>
     );
 };
