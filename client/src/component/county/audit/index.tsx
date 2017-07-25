ef90ee96bece9b8dc77a7fabc7c36b058d4d3f50
Joe Ranweiler <joe@lemma.co>
2017-07-25 16:01:03 -0700

    Render county audit page in container.

---
+++ client/src/component/county/audit/index.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+import CountyNav from '../Nav';
+
+
+const CountyAuditPage = () => {
+    return (
+        <div>
+            <CountyNav />
+            <div>Audit root.</div>
+        </div>
+    );
+};
+
+export default CountyAuditPage;
--- client/src/container/county/Audit.tsx
+++ client/src/container/county/Audit.tsx
@@ -1,12 +1,12 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
 
+import CountyAuditPage from '../../component/county/audit';
+
 
 class CountyAuditContainer extends React.Component<any, any> {
     public render() {
-        return (
-            <div>County Audit</div>
-        );
+        return <CountyAuditPage />;
     }
 }
 
