5046e7308aa6d13c2e13b22613997e8ad40d4a3b
Joe Ranweiler <joe@lemma.co>
2017-07-24 21:30:46 -0700

    Add example app state for dev, make default.

--- client/src/reducer/root.ts
+++ client/src/reducer/root.ts
@@ -13,7 +13,44 @@ const defaultState = {
     loggedIn: false,
 };
 
-export default function root(state: AppState = defaultState, action: any) {
+// tslint:disable
+const exampleState = {
+    loggedIn: false,
+    county: {
+        name: 'County Name',
+        info: {
+            field1: 'abc',
+            field2: 'xyz',
+        },
+        contests: [
+            {
+                name: 'Contest A',
+                choices: [
+                    { id: 1, name: 'Candidate 1' },
+                    { id: 2, name: 'Candidate 2' },
+                    { id: 3, name: 'Candidate 3' },
+                ],
+            },
+            {
+                name: 'Contest B',
+                choices: [
+                    { id: 4, name: 'Candidate 4' },
+                    { id: 5, name: 'Candidate 5' },
+                ],
+            },
+            {
+                name: 'Contest C',
+                choices: [
+                    { id: 7, name: 'Candidate 7' },
+                    { id: 8, name: 'Candidate 8' },
+                ],
+            },
+        ],
+    },
+};
+// tslint:enable
+
+export default function root(state: AppState = exampleState, action: any) {
     switch (action.type) {
     default:
         return state;
