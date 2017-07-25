fb3a355a1a217213d7c19032fe4751b49e0c63f5
Joe Ranweiler <joe@lemma.co>
2017-07-24 21:13:18 -0700

    Remove scratch component and app state.

--- client/src/reducer/root.ts
+++ client/src/reducer/root.ts
@@ -1,23 +1,13 @@
 interface AppState {
-    greeting: string;
     loggedIn: boolean;
 }
 
 const defaultState = {
-    greeting: 'Hello',
     loggedIn: false,
 };
 
 export default function root(state: AppState = defaultState, action: any) {
     switch (action.type) {
-
-    case 'NEXT_GREETING':
-        const greeting
-            = state.greeting === 'Hello'
-            ? 'Hi'
-            : 'Hello';
-        return { greeting };
-
     default:
         return state;
     }
