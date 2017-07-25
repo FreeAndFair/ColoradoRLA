b98650531bf1cdca10072ee436b19704233b46ed
Joe Ranweiler <joe@lemma.co>
2017-07-24 20:10:19 -0700

    Draft layout of county home page.

--- client/src/component/county/index.tsx
+++ client/src/component/county/index.tsx
@@ -3,11 +3,46 @@ import * as React from 'react';
 import CountyNav from './Nav';
 
 
+const Main = () => (
+    <div>
+        <div>Your status:</div>
+        <div>
+            <div>Please upload your Ballot Manifest and Cast Vote Records.</div>
+            <div>
+                Ballot Manifest:
+                <button className='pt-button pt-intent-primary pt-icon-add'>
+                    Upload
+                </button>
+            </div>
+            <div>
+                Cast Vote Records:
+                <button className='pt-button pt-intent-primary pt-icon-add'>
+                    Upload
+                </button>
+            </div>
+        </div>
+    </div>
+);
+
+const ContestInfo = () => (
+    <div>Contest info</div>
+);
+
+const CountyInfo = () => (
+    <div>County info</div>
+);
+
+
 export default class CountyRootPage extends React.Component<any, any> {
     public render() {
         return (
             <div>
                 <CountyNav />
+                <div>
+                    <Main />
+                    <CountyInfo />
+                    <ContestInfo />
+                </div>
             </div>
         );
     }
