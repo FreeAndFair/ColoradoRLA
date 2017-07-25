a90f4b01179ed341b13dd9a10b5d6672296ce74f
Daniel M. Zimmerman <dmz@freeandfair.us>
2017-07-25 13:59:31 -0700

    Cleaned up static analysis issues.

--- server/eclipse-project/src/main/java/us/freeandfair/corla/model/BallotManifestInfo.java
+++ server/eclipse-project/src/main/java/us/freeandfair/corla/model/BallotManifestInfo.java
@@ -12,114 +12,112 @@
 package us.freeandfair.corla.model;
 
 /**
- * @description <description>
- * @explanation <explanation>
- * @bon OPTIONAL_BON_TYPENAME
+ * Information about a specific ballot on a ballot manifest.
+ * 
+ * @author Joey Dodds
+ * @version 0.0.1
  */
 public class BallotManifestInfo {
+  /**
+   * The ID number of the county in which the ballot was cast.
+   */
   private final int my_county_id;
-  private final int my_scanner_id_number;
+  //@ private invariant my_county_id > 0;
+  
+  /**
+   * The ID number of the scanner that scanned the ballot.
+   */
+  private final int my_scanner_id;
+
+  /**
+   * The batch number containing this ballot.
+   */
+  private final int my_batch_number;
+  
+  /**
+   * The size of the ballot batch containing this ballot.
+   */
   private final int my_batch_size;
+  
+  /**
+   * The storage container number containing this ballot.
+   */
   private final int my_storage_container;
  
   /**
+   * The ballot ID of this ballot.
+   */
+  private final int my_ballot_id;
+  
+  /**
    * <description>
    * <explanation>
    * @param
-   * @return the my_county_id
    */
-  //@ behavior
-  //@   requires P;
-  //@   ensures Q;
-  /*@ pure @*/
+  public BallotManifestInfo(final int the_county_id, final int the_scanner_id, 
+                            final int the_batch_number, final int the_batch_size, 
+                            final int the_storage_container, final int the_ballot_id) {
+    super();
+    my_county_id = the_county_id;
+    my_scanner_id = the_scanner_id;
+    my_batch_number = the_batch_number;
+    my_batch_size = the_batch_size;
+    my_storage_container = the_storage_container;
+    my_ballot_id = the_ballot_id;
+  }
   
-  public int getMy_county_id() {
+  /**
+   * @return the county ID.
+   */
+  public int getCountyID() {
     assert false;
     //@ assert false;
     return my_county_id;
-  }
-  /* (non-Javadoc)
-   * @see java.lang.Object#toString()
-   */
-  //@ also behavior
-  //@ behavior
-  //@   requires P;
-  //@   ensures Q;
-  /*@ pure @*/
+  }  
 
-  
-  
-  @Override
-  public String toString() {
-    return "BallotManifestInfo [my_county_id=" + my_county_id + ", my_scanner_id_number=" +
-           my_scanner_id_number + ", my_batch_size=" + my_batch_size +
-           ", my_storage_container=" + my_storage_container + "]";
-  }
   /**
-   * <description>
-   * <explanation>
-   * @param
+   * @return the scanner ID.
    */
-  //@ behavior
-  //@   requires P;
-  //@   ensures Q;
-  /*@ pure @*/
-  
-  public BallotManifestInfo(int my_county_id, int my_scanner_id_number, int my_batch_size,
-                            int my_storage_container) {
-    super();
-    this.my_county_id = my_county_id;
-    this.my_scanner_id_number = my_scanner_id_number;
-    this.my_batch_size = my_batch_size;
-    this.my_storage_container = my_storage_container;
+  public int getScannerID() {
+    return my_scanner_id;
   }
-  /**
-   * <description>
-   * <explanation>
-   * @param
-   * @return the my_scanner_id_number
-   */
-  //@ behavior
-  //@   requires P;
-  //@   ensures Q;
-  /*@ pure @*/
   
-  public int getMy_scanner_id_number() {
-    assert false;
-    //@ assert false;
-    return my_scanner_id_number;
+  /**
+   * @return the batch number.
+   */
+  public int getBatchNumber() {
+    return my_batch_number;
   }
-  /**
-   * <description>
-   * <explanation>
-   * @param
-   * @return the my_batch_size
-   */
-  //@ behavior
-  //@   requires P;
-  //@   ensures Q;
-  /*@ pure @*/
   
-  public int getMy_batch_size() {
-    assert false;
-    //@ assert false;
+  /**
+   * @return the batch size.
+   */
+  public int getBatchSize() {
     return my_batch_size;
   }
-  /**
-   * <description>
-   * <explanation>
-   * @param
-   * @return the my_storage_container
-   */
-  //@ behavior
-  //@   requires P;
-  //@   ensures Q;
-  /*@ pure @*/
   
-  public int getMy_storage_container() {
-    assert false;
-    //@ assert false;
+  /**
+   * @return the storage container number.
+   */
+  public int getStorageContainer() {
     return my_storage_container;
   }  
   
+  /**
+   * @return the ballot ID.
+   */
+  public int getBallotID() {
+    return my_ballot_id;
+  }
+  
+  /**
+   * @return a String representation of this object.
+   */
+  @Override
+  public String toString() {
+    return "BallotManifestInfo [county_id=" + my_county_id + 
+        ", scanner_id=" + my_scanner_id + ", batch_size=" + my_batch_size +
+        ", storage_container=" + my_storage_container + 
+        ", ballot_id=" + my_ballot_id + "]";
+  }
 }
