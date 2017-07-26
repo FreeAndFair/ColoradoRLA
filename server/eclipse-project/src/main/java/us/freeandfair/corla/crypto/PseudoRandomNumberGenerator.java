aa48e94e65fd71913460ae037f4091bcdf8b6e9d
Daniel M. Zimmerman <dmz@freeandfair.us>
2017-07-26 10:23:03 -0700

    Added PRNG class.

---
+++ server/eclipse-project/src/main/java/us/freeandfair/corla/crypto/PseudoRandomNumberGenerator.java
@@ -0,0 +1,27 @@
+/*
+ * Free & Fair Colorado RLA System
+ * 
+ * @title ColoradoRLA
+ * @created Jul 25, 2017
+ * @copyright 2017 Free & Fair
+ * @license GNU General Public License 3.0
+ * @author Daniel M. Zimmerman <dmz@freeandfair.us>
+ * @description A system to assist in conducting statewide risk-limiting audits.
+ */
+
+package us.freeandfair.corla.crypto;
+
+/**
+ * A pseudo-random number generator based on Philip Stark's pseudo-random
+ * number generator found at 
+ * <a href="https://www.stat.berkeley.edu/~stark/Java/Html/sha256Rand.htm">
+ * https://www.stat.berkeley.edu/~stark/Java/Html/sha256Rand.htm</a>.
+ * 
+ * @author Daniel M. Zimmerman
+ * @version 0.0.1
+ */
+public class PseudoRandomNumberGenerator {
+  /**
+   * The current 
+   */
+}
