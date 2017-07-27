863ed4d71f36838202f68b0428f6bb4ddfeb6809
Joe Ranweiler <joe@lemma.co>
2017-07-27 00:35:54 -0700

    Remove start stage of county audit wizard.

--- client/src/component/county/audit/wizard/CountyAuditWizard.tsx
+++ client/src/component/county/audit/wizard/CountyAuditWizard.tsx
@@ -3,10 +3,9 @@ import * as React from 'react';
 import AuditBoardSignInStage from './AuditBoardSignInStage';
 import BallotAuditStage from './BallotAuditStage';
 import ReviewStage from './ReviewStage';
-import StartStage from './StartStage';
 
 
-type WizardStage = 'start' | 'sign-in' | 'ballot-audit' | 'review';
+type WizardStage = 'sign-in' | 'ballot-audit' | 'review';
 
 interface CountyAuditWizardState {
     stage: WizardStage;
@@ -16,16 +15,13 @@ class CountyAuditWizard extends React.Component<any, CountyAuditWizardState> {
     constructor(props: any) {
         super(props);
 
-        this.state = { stage: 'start' };
+        this.state = { stage: 'sign-in' };
     }
 
     public render() {
         let nextStage;
 
         switch (this.state.stage) {
-            case 'start':
-                nextStage = () => this.setState({ stage: 'sign-in' });
-                return <StartStage nextStage={ nextStage } />;
             case 'sign-in':
                 nextStage = () => this.setState({ stage: 'ballot-audit' });
                 return <AuditBoardSignInStage nextStage={ nextStage } />;
