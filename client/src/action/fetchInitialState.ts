debda4bf2d00bd7c5aaad3462ac973e26f83e80f
Joe Ranweiler <joe@lemma.co>
2017-07-28 16:17:31 -0700

    Create initial state after fetch.

--- client/src/action/fetchInitialState.ts
+++ client/src/action/fetchInitialState.ts
@@ -1,5 +1,11 @@
+import * as Promise from 'bluebird';
+
 import { exampleState } from '../example';
 
+
+function createInitialState({ ballotStyles, castVoteRecords, contests }: any) {
+    return {};
+}
 
 export default function fetchInitialState(dispatch: any) {
     dispatch({ type: 'FETCH_INITIAL_STATE_SEND' });
@@ -11,4 +17,22 @@ export default function fetchInitialState(dispatch: any) {
 
     // Simulate `fetch`.
     setTimeout(receive, 100);
+
+    const fetchBallotStyles = fetch('http://localhost:4000/ballot-style')
+        .then((res: any) => res.json());
+
+    const fetchCastVoteRecords =  fetch('http://localhost:4000/cvr')
+        .then((res: any) => res.json());
+
+    const fetchContests = fetch('http://localhost:4000/contest')
+        .then((res: any) => res.json());
+
+    Promise.all([fetchBallotStyles, fetchCastVoteRecords, fetchContests])
+        .then((res: any) => {
+            const [ballotStyles, castVoteRecords, contests] = res;
+
+            const data =  createInitialState({ ballotStyles, castVoteRecords, contests });
+
+            dispatch({ data, type: 'FETCH_INITIAL_STATE_RECEIVE' });
+        });
 }
