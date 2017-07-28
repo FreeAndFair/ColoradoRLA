4306b9759247bc4655f3a0b7efc64702993a0476
Joe Ranweiler <joe@lemma.co>
2017-07-27 22:31:12 -0700

    Add audit board example state, pass as props to sign-in stage.

--- client/src/component/county/audit/wizard/AuditBoardSignInStage.tsx
+++ client/src/component/county/audit/wizard/AuditBoardSignInStage.tsx
@@ -3,41 +3,67 @@ import * as React from 'react';
 import { EditableText, Radio, RadioGroup } from '@blueprintjs/core';
 
 
-const AuditBoardSignInForm = () => (
-    <div>
-        <h3>Audit Board Member</h3>
-        <div className='pt-card'>
-            <label>Full Name:
-                <EditableText />
-            </label>
-        </div>
-        <div className='pt-card'>
-            <RadioGroup label='Party Affiliation' onChange={ () => ({}) }>
-                <Radio label='Democratic Party' />
-                <Radio label='Republican Party' />
-                <Radio label='Minor Party' />
-                <Radio label='Unaffiliated' />
-            </RadioGroup>
-        </div>
-    </div>
-);
+const AuditBoardSignInForm = (props: any) => {
+    const { boardMember } = props;
+    const { name, party } = boardMember;
 
-const AuditBoardSignInStage = ({ nextStage }: any) => (
-    <div>
+    return (
         <div>
-            <h2>Audit Board Sign-in</h2>
-            <p>Enter the full names and party affiliations of each member of
-                the Acme County Audit Board who will be conducting this audit
-                today:
-            </p>
+            <h3>Audit Board Member</h3>
+            <div className='pt-card'>
+                <label>Full Name:
+                    <EditableText value={ name } />
+                </label>
+            </div>
+            <div className='pt-card'>
+                <RadioGroup
+                    label='Party Affiliation'
+                    onChange={ () => ({}) }
+                    selectedValue={ party }
+                >
+                    <Radio
+                        label='Democratic Party'
+                        value='Democratic Party'
+                    />
+                    <Radio
+                        label='Republican Party'
+                        value='Republican Party'
+                    />
+                    <Radio
+                        label='Other Party'
+                        value='Other Party'
+                    />
+                    <Radio
+                        label='Unaffiliated'
+                        value='Unaffiliated'
+                    />
+                </RadioGroup>
+            </div>
         </div>
-        <AuditBoardSignInForm />
-        <AuditBoardSignInForm />
-        <button className='pt-button pt-intent-primary' onClick={ nextStage }>
-            Next
-        </button>
-    </div>
-);
+    );
+};
+
+const AuditBoardSignInStage = (props: any) => {
+    const { county, nextStage } = props;
+    const { auditBoard } = county;
+
+    return (
+        <div>
+            <div>
+                <h2>Audit Board Sign-in</h2>
+                <p>Enter the full names and party affiliations of each member of
+                    the Acme County Audit Board who will be conducting this audit
+                    today:
+                </p>
+            </div>
+            <AuditBoardSignInForm boardMember={ auditBoard[0] } />
+            <AuditBoardSignInForm boardMember={ auditBoard[1] } />
+            <button className='pt-button pt-intent-primary' onClick={ nextStage }>
+                Next
+            </button>
+        </div>
+    );
+};
 
 
 export default AuditBoardSignInStage;
