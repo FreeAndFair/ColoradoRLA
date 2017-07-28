d0c3663db224aa9d3691e364bf8bfb2e5e97a9ef
Joe Ranweiler <joe@lemma.co>
2017-07-28 13:36:31 -0700

    Add login page styling.

--- client/src/component/login/LoginPage.tsx
+++ client/src/component/login/LoginPage.tsx
@@ -6,7 +6,8 @@ import LoginFormContainer from './LoginFormContainer';
 export default class LoginPage extends React.Component<any, any> {
     public render() {
         return (
-            <div>
+            <div className='pt-card login-page'>
+                <h2>County User Login</h2>
                 <LoginFormContainer />
             </div>
         );
