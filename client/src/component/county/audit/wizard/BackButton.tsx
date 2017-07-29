8ea9dbb327fda3bceb16cb6327396c561df5ac6f
Joe Ranweiler <joe@lemma.co>
2017-07-28 18:37:34 -0700

    Factor out back button, add to ballot audit stage of wizard.

---
+++ client/src/component/county/audit/wizard/BackButton.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+
+const BackButton = ({ back }: any) => {
+    return <button className='pt-button' onClick={ back }>Back</button>;
+};
+
+
+export default BackButton;
