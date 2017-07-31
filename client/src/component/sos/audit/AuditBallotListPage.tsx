ba3d3546c73a3efc6acababa0c5a9cdef1d40cd8
Joe Ranweiler <joe@lemma.co>
2017-07-31 16:41:16 -0700

    Add instructions, ballot manifest upload form to SoS audit page.

--- client/src/component/sos/audit/AuditBallotListPage.tsx
+++ client/src/component/sos/audit/AuditBallotListPage.tsx
@@ -33,7 +33,20 @@ const Audit = () => {
             <Breadcrumb />
             <h2>Administer an Audit</h2>
             <h3>Audit Definition - Ballot List</h3>
-
+            <div>
+                This is the list of ballots to audit generated from the random seed.
+                Once this is submitted, it will be released to the counties and the
+                previous pages will not be editable.
+            </div>
+            <div className='pt-card'>
+                Ballot List:
+                <div>
+                    <label className='pt-file-upload'>
+                        <input type='file' onChange={ nop } />
+                        <span className='pt-file-upload-input'>AcmeCountyBallotManifest.csv</span>
+                    </label>
+                </div>
+            </div>
             <div>
                 <button className='pt-button'>Back</button>
                 <button className='pt-button pt-intent-primary'>Launch Audit</button>
