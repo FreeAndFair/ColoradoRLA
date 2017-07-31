be2f88adc8a3d79b36fc9308a10225b7b5eff0e6
Joe Ranweiler <joe@lemma.co>
2017-07-31 14:06:56 -0700

    Add breadcrumb to SoS county overview page.

--- client/src/component/sos/county/CountyOverviewPage.tsx
+++ client/src/component/sos/county/CountyOverviewPage.tsx
@@ -3,10 +3,26 @@ import * as React from 'react';
 import Nav from '../Nav';
 
 
+const Breadcrumb = () => (
+    <ul className='pt-breadcrumbs'>
+        <li>
+            <a className='pt-breadcrumb' href='/sos'>
+                SoS
+            </a>
+        </li>
+        <li>
+            <a className='pt-breadcrumb pt-breadcrumb-current'>
+                Counties
+            </a>
+        </li>
+    </ul>
+);
+
 const CountyOverviewPage = (props: any) => {
     return (
         <div>
             <Nav />
+            <Breadcrumb />
             <div>
                 County overview.
             </div>
