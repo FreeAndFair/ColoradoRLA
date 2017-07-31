1ffcd1e3b9a00255ee428579aea2943381f37eed
Joe Ranweiler <joe@lemma.co>
2017-07-31 12:54:43 -0700

    Enrich contest detail page.

--- client/src/component/sos/contest/ContestDetailPage.tsx
+++ client/src/component/sos/contest/ContestDetailPage.tsx
@@ -31,8 +31,25 @@ const ContestDetailPage = (props: any) => {
         <div>
             <Nav />
             <Breadcrumb contest={ contest } />
+            <h2>Status</h2>
+            <h3>County Data</h3>
+            <div>Ballot manifest: uploaded</div>
+            <div>CVR export: uploaded</div>
+            <div>Projected start date: 11/15/2017</div>
             <div>
-                Contest details.
+                <h3>Progress</h3>
+                <div>
+                    Audit board:
+                    <div>John Doe (Democratic Party)</div>
+                    <div>Jane Smith (RepublicanParty)</div>
+                </div>
+                <div>
+                    <div>Round 1</div>
+                    <div>Start time: 11/15/2016, 11:00 a.m. MST</div>
+                    <div>Ballots submitted: 1001</div>
+                    <div>Discrepancies: 3</div>
+                </div>
+
             </div>
         </div>
     );
