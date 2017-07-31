d9e03fef40b561af6a6e196a5a2dc05557392ec3
Joe Ranweiler <joe@lemma.co>
2017-07-31 15:56:48 -0700

    Outline fields in SoS audit admin page.

--- client/src/component/sos/audit/AuditPage.tsx
+++ client/src/component/sos/audit/AuditPage.tsx
@@ -24,7 +24,22 @@ const Audit = () => {
         <div>
             <Nav />
             <Breadcrumb />
-            <h2>Audit Admin</h2>
+            <h2>Administer an Audit</h2>
+            <h3>Audit Definition</h3>
+            <div>Enter the date the election will take place, and the type of election.</div>
+            <div>
+                Election date.
+            </div>
+            <div>
+                Election type.
+            </div>
+            <h3>Risk Limits</h3>
+            <div>Each contest type has a default risk limit set. To change the risk limit for a
+                class, change the percentage shown. To change the risk limit for a particular
+                contest use the link below to add an exception from the default.
+            </div>
+            <h4>Ballot Polling Audits</h4>
+            <h4>Comparison Audits</h4>
         </div>
     );
 };
