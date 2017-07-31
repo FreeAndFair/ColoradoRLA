9a93b830eeabf06c24d829544f298fc730393129
Joe Ranweiler <joe@lemma.co>
2017-07-31 16:30:13 -0700

    Add audit seed form.

--- client/src/component/sos/audit/AuditSeedPage.tsx
+++ client/src/component/sos/audit/AuditSeedPage.tsx
@@ -28,7 +28,16 @@ const Audit = () => {
         <div>
             <Nav />
             <Breadcrumb />
-            <div>Seed.</div>
+            <div>
+                <h3>Audit Definition - Enter Random Seed</h3>
+                <div>Enter the random seed generated from the public meeting on 11/10/2017.</div>
+
+                <h4>Audit Random Seed</h4>
+                <div>Please enter the seed generated from the public meeting.</div>
+                <label>Seed
+                    <input className='pt-input' type='text' />
+                </label>
+            </div>
         </div>
     );
 };
