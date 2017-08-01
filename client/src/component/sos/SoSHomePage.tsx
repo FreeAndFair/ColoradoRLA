26e82eb15a8a9d6622c387579581d200d9c67b4b
Joe Ranweiler <joe@lemma.co>
2017-07-31 17:42:01 -0700

    Fix layout of SoS home page.

--- client/src/component/sos/SoSHomePage.tsx
+++ client/src/component/sos/SoSHomePage.tsx
@@ -3,47 +3,75 @@ import * as React from 'react';
 import SoSNav from './Nav';
 
 
-const SoSHomePage = () => {
+const AuditRounds = () => {
     return (
         <div>
-            <SoSNav />
+            List of Audit Rounds (number of ballots, status by County, download links)
+        </div>
+    );
+};
+
+const SeedInfo = () => {
+    return (
+        <div>
+        </div>
+    );
+};
+
+const ContestUpdates = () => {
+    return (
+        <div className='pt-card'>
+            <h3>Contest updates.</h3>
             <div>
-                Notifications.
+                Target and Current Risk Limits by Contest
             </div>
             <div>
-                <div>
-                    <h3>County Updates</h3>
-                    <div>
-                        County Ballot Manifests, CVRs and Hashes (status & download links)
-                    </div>
-                    <div>
-                        [Ballot Order]
-                        [For each audited contest, total number of ballots to be audited
-                        (including previous rounds)]
-                    </div>
-                    <div>
-                        Updates based on county progress, discrepencies.
-                    </div>
-                </div>
-                <div>
-                    <h3>Contest updates.</h3>
-                    <div>
-                        Target and Current Risk Limits by Contest
-                    </div>
-                    <div>
-                        Status (audit required, audit in progress, audit complete, hand
-                        count required, hand count complete) by Audited Contest
-                    </div>
-                    <div>
-                        [Seed for randomization]
-                    </div>
-                    <div>
-                        List of Audit Rounds (number of ballots, status by County, download links)
-                    </div>
-                    <div>
-                        Link to Final Audit Report
-                    </div>
-                </div>
+                Status (audit required, audit in progress, audit complete, hand
+                count required, hand count complete) by Audited Contest
+            </div>
+            <div>
+                Seed: 23781234
+            </div>
+            <AuditRounds />
+        </div>
+    );
+};
+
+const CountyUpdates = () => {
+    return (
+        <div className='pt-card'>
+            <h3>County Updates</h3>
+            <div>
+                County Ballot Manifests, CVRs and Hashes (status & download links)
+            </div>
+            <div>
+                [Ballot Order]
+                [For each audited contest, total number of ballots to be audited
+                (including previous rounds)]
+            </div>
+            <div>
+                Updates based on county progress, discrepencies.
+            </div>
+        </div>
+    );
+};
+
+
+const SoSHomePage = (props: any) => {
+    return (
+        <div className='sos-home'>
+            <SoSNav />
+            <div className='sos-notifications pt-card'>
+                <em>No notifications.</em>
+            </div>
+            <div className='sos-info pt-card'>
+                <CountyUpdates />
+                <ContestUpdates />
+            </div>
+            <div>
+                <button disabled className='pt-button pt-intent-primary'>
+                    Final Audit Report
+                </button>
             </div>
         </div>
     );
