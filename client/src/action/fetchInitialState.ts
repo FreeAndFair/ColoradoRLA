7d362a399af8fe0bb17ab2dfc2ab85d78577ab82
Joe Ranweiler <joe@lemma.co>
2017-08-08 13:39:58 -0700

    Create initial client state after fetch.

--- client/src/action/fetchInitialState.ts
+++ client/src/action/fetchInitialState.ts
@@ -1,14 +1,27 @@
-import { exampleState } from '../example';
+function createInitialState(data: any) {
+    const state = { ...data };
 
+    return state;
+}
 
 export default function fetchInitialState(dispatch: any) {
     dispatch({ type: 'FETCH_INITIAL_STATE_SEND' });
 
-    const receive = () => dispatch({
-        data: exampleState,
-        type: 'FETCH_INITIAL_STATE_RECEIVE',
-    });
+    const fetchBallotStyles = fetch('http://localhost:4000/ballot-style')
+        .then((res: any) => res.json());
 
-    // Simulate `fetch`.
-    setTimeout(receive, 100);
+    const fetchCastVoteRecords = fetch('http://localhost:4000/cvr')
+        .then((res: any) => res.json());
+
+    const fetchContests = fetch('http://localhost:4000/contest')
+        .then((res: any) => res.json());
+
+    Promise.all([fetchBallotStyles, fetchCastVoteRecords, fetchContests])
+        .then((res: any) => {
+            const [ballotStyles, castVoteRecords, contests] = res;
+
+            const data = createInitialState({ ballotStyles, castVoteRecords, contests });
+
+            dispatch({ data, type: 'FETCH_INITIAL_STATE_RECEIVE' });
+        });
 }
