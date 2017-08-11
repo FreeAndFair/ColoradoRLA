f666cbd54544d482a0dfda910ff3668dc960ff5d
Joe Kiniry <kiniry@freeandfair.us>
2017-08-11 00:53:58 -0400

    Removed the automatic application of final on private fields.
    Per Dan's request on Slack.

--- server/eclipse-project/src/main/java/us/freeandfair/corla/endpoint/AuthenticateStateAdministrator.java
+++ server/eclipse-project/src/main/java/us/freeandfair/corla/endpoint/AuthenticateStateAdministrator.java
@@ -1,6 +1,6 @@
 /*
  * Free & Fair Colorado RLA System
- * 
+ *
  * @title ColoradoRLA
  * @created Aug 9, 2017
  * @copyright 2017 Free & Fair
@@ -23,7 +23,7 @@ import us.freeandfair.corla.model.Administrator.AdministratorType;
 
 /**
  * The endpoint for authenticating a state administrator.
- * 
+ *
  * @author Daniel M Zimmerman
  * @version 0.0.1
  */
@@ -36,7 +36,7 @@ public class AuthenticateStateAdministrator implements Endpoint {
   public EndpointType endpointType() {
     return EndpointType.POST;
   }
-  
+
   /**
    * {@inheritDoc}
    */
@@ -48,10 +48,10 @@ public class AuthenticateStateAdministrator implements Endpoint {
   /**
    * Attempts to authenticate a state administrator; if the authentication is
    * successful, authentication data is added to the session.
-   * 
-   * Session query parameters: <tt>username</tt>, <tt>password</tt>, 
+   *
+   * Session query parameters: <tt>username</tt>, <tt>password</tt>,
    * <tt>second_factor</tt>
-   * 
+   *
    * @param the_request The request.
    * @param the_response The response.
    */
@@ -59,7 +59,7 @@ public class AuthenticateStateAdministrator implements Endpoint {
   public String endpoint(final Request the_request, final Response the_response) {
     int status = HttpStatus.OK_200;
     String result = "Authenticated";
-    
+
     if (!Authentication.authenticateAs(the_request, AdministratorType.STATE)) {
       status = HttpStatus.UNAUTHORIZED_401;
       result = "Authentication failed";
@@ -67,8 +67,8 @@ public class AuthenticateStateAdministrator implements Endpoint {
     // Take the transition triggered by this successful authentication.
     Main.asm().stepEvent(DosDashboardEvent.AUTHENTICATE_STATE_ADMINISTRATOR_EVENT);
     the_response.status(status);
-    // Build the ASM server response. 
-    final ServerAsmResponse asm_response = 
+    // Build the ASM server response.
+    final ServerAsmResponse asm_response =
         new ServerAsmResponse(Main.asm().currentState(),
                               Main.asm().enabledUiEvents());
     the_response.body(Main.GSON.toJson(asm_response));
