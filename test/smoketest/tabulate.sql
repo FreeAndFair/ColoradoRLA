e256c1c284cf7d992150ce0dabda15c18c754b9b
Neal McBurnett <nealmcb@freeandfair.us>
2017-08-10 23:30:26 -0600

    Get smoketest working with latest master.
    Add tabulation sql query and test.
    And flesh out documentation for updating tests.

---
+++ test/smoketest/tabulate.sql
@@ -0,0 +1,15 @@
+SELECT ccd.contest_id AS contest, 
+        ccd.choice AS candidate, 
+        count(ccic.cvr_contest_info_id) as votes
+    FROM contest_choice_description AS ccd 
+        LEFT JOIN ((contest AS c 
+            LEFT JOIN cvr_contest_info AS cci
+                ON c.id = cci.contest_id)
+        LEFT JOIN 
+            cvr_contest_info_choice as ccic 
+            ON ccic.cvr_contest_info_id = cci.id)
+            ON ccd.contest_id = c.id 
+        AND ccd.choice = ccic.choice
+    GROUP BY contest, candidate
+    ORDER BY contest, candidate
+;
