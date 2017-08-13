5f23317741556f5196bb4fedd29c0d406ceeb36d
Joe Kiniry <kiniry@freeandfair.us>
2017-08-12 21:08:22 -0400

    Added support for skip and refresh events from any state.

--- server/eclipse-project/src/main/java/us/freeandfair/corla/asm/AbstractStateMachine.java
+++ server/eclipse-project/src/main/java/us/freeandfair/corla/asm/AbstractStateMachine.java
@@ -12,6 +12,10 @@
 
 package us.freeandfair.corla.asm;
 
+import static us.freeandfair.corla.asm.ASMEvent.AuditBoardDashboardEvent.*;
+import static us.freeandfair.corla.asm.ASMEvent.CountyDashboardEvent.*;
+import static us.freeandfair.corla.asm.ASMEvent.DoSDashboardEvent.*;
+
 import java.io.Serializable;
 import java.util.HashSet;
 import java.util.List;
@@ -176,6 +180,13 @@ public abstract class AbstractStateMachine implements Serializable {
    */
   public Set<ASMEvent> enabledASMEvents() {
     final Set<ASMEvent> result = new HashSet<ASMEvent>();
+    // skips and refreshes are always permitted
+    result.add(DOS_SKIP_EVENT);
+    result.add(DOS_REFRESH_EVENT);
+    result.add(COUNTY_SKIP_EVENT);
+    result.add(COUNTY_REFRESH_EVENT);
+    result.add(AUDIT_SKIP_EVENT);
+    result.add(AUDIT_REFRESH_EVENT);
     for (final ASMTransition t : my_transition_function) {
       if (t.startState().equals(my_current_state)) {
         result.add(t.event());
@@ -217,6 +228,16 @@ public abstract class AbstractStateMachine implements Serializable {
    */
   public ASMState stepEvent(final ASMEvent the_event)
       throws IllegalStateException {
+    // refresh and skip events are always permitted
+    if (the_event == DOS_REFRESH_EVENT ||
+        the_event == DOS_SKIP_EVENT ||
+        the_event == COUNTY_REFRESH_EVENT || 
+        the_event == COUNTY_SKIP_EVENT ||
+        the_event == AUDIT_REFRESH_EVENT || 
+        the_event == AUDIT_SKIP_EVENT) {
+      return currentState();
+    }
+                
     ASMState result = null;
     for (final ASMTransition t : my_transition_function) {
       if (t.startState().equals(my_current_state) &&
