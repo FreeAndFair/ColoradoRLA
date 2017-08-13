68dc3775bba29d61e922dc0786eef62acea2c815
Joe Ranweiler <joe@lemma.co>
2017-08-13 11:44:34 -0700

    Add `createSubmitAction` for async HTTP submit actions.

---
+++ client/src/action/createSubmitAction.ts
@@ -0,0 +1,34 @@
+import { Dispatch } from 'redux';
+
+import { apiHost } from '../config';
+
+
+const createSubmitAction = ({
+    failType,
+    networkFailType,
+    okType,
+    sendType,
+    url,
+}: any) => (body: any) => {
+    return (dispatch: Dispatch<any>) => {
+        dispatch({ type: sendType });
+
+        fetch(url, { method: 'post', body })
+            .then(r => {
+                if (!r.ok) {
+                    dispatch({ type: failType });
+                    return;
+                }
+
+                r.json().then((data: any) => {
+                    dispatch({ type: okType, data });
+                });
+            })
+            .catch(() => {
+                dispatch({ type: networkFailType });
+            });
+    };
+};
+
+
+export default createSubmitAction;
