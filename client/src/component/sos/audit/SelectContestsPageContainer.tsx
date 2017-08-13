ac230ed3e9fa42e987373d6a376eb73f0632f52a
Joe Ranweiler <joe@lemma.co>
2017-08-12 23:00:21 -0700

    Add contest selection page, form, and routing.

---
+++ client/src/component/sos/audit/SelectContestsPageContainer.tsx
@@ -0,0 +1,38 @@
+import * as React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators, Dispatch } from 'redux';
+
+import SelectContestsPage from './SelectContestsPage';
+
+import selectContestsForAudit from '../../../action/selectContestsForAudit';
+
+
+class SelectContestsPageContainer extends React.Component<any, any> {
+    public render() {
+        const { history, contests, selectContestsForAudit } = this.props;
+
+        const props = {
+            back: () => history.push('/sos/audit'),
+            contests,
+            nextPage: () => history.push('/sos/audit/ballots'),
+            selectContestsForAudit,
+        };
+
+        return <SelectContestsPage { ...props } />;
+    }
+}
+
+const mapStateToProps = ({ sos }: any) => ({
+    contests: sos.contests,
+    sos,
+});
+
+const mapDispatchToProps = (dispatch: Dispatch<any>) => bindActionCreators({
+    selectContestsForAudit,
+}, dispatch);
+
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps,
+)(SelectContestsPageContainer);
