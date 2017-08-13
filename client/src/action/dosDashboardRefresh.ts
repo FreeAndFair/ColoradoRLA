b6caa0cb11139e5e0ebbe25dcc01d2008987177b
Joe Ranweiler <joe@lemma.co>
2017-08-13 11:28:24 -0700

    Rename fetch `_RECEIVE` action types to `_OK`.

--- client/src/action/dosDashboardRefresh.ts
+++ client/src/action/dosDashboardRefresh.ts
@@ -10,7 +10,7 @@ const url = `http://${apiHost}/dos-dashboard`;
 const dosDashboardRefresh = createFetchAction({
     failType: 'DOS_DASHBOARD_REFRESH_FAIL',
     networkFailType: 'DOS_DASHBOARD_REFRESH_NETWORK_FAIL',
-    okType: 'DOS_DASHBOARD_REFRESH_RECEIVE',
+    okType: 'DOS_DASHBOARD_REFRESH_OK',
     sendType: 'DOS_DASHBOARD_REFRESH_SEND',
     url,
 });
