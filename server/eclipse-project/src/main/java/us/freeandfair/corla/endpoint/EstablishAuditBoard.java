e9e45562509ae71946ff69c4e34068c7fb4cc3cd
Joe Kiniry <kiniry@freeandfair.us>
2017-08-12 22:24:53 -0400

    Impls of all missing endpoints for County Dashboard.

---
+++ server/eclipse-project/src/main/java/us/freeandfair/corla/endpoint/EstablishAuditBoard.java
@@ -0,0 +1,100 @@
+/*
+ * Free & Fair Colorado RLA System
+ * 
+ * @title ColoradoRLA
+ * @created Jul 27, 2017
+ * @copyright 2017 Free & Fair
+ * @license GNU General Public License 3.0
+ * @author Daniel M. Zimmerman <dmz@freeandfair.us>
+ * @description A system to assist in conducting statewide risk-limiting audits.
+ */
+
+package us.freeandfair.corla.endpoint;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.sql.Blob;
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.OptionalLong;
+
+import javax.persistence.PersistenceException;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.fileupload.FileItemIterator;
+import org.apache.commons.fileupload.FileItemStream;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.fileupload.util.Streams;
+import org.eclipse.jetty.http.HttpStatus;
+import org.hibernate.Session;
+
+import spark.Request;
+import spark.Response;
+
+import us.freeandfair.corla.Main;
+import us.freeandfair.corla.csv.BallotManifestParser;
+import us.freeandfair.corla.csv.ColoradoBallotManifestParser;
+import us.freeandfair.corla.model.BallotManifestInfo;
+import us.freeandfair.corla.model.County;
+import us.freeandfair.corla.model.CountyDashboard;
+import us.freeandfair.corla.model.UploadedFile;
+import us.freeandfair.corla.model.UploadedFile.FileType;
+import us.freeandfair.corla.model.UploadedFile.HashStatus;
+import us.freeandfair.corla.persistence.Persistence;
+import us.freeandfair.corla.query.CountyDashboardQueries;
+import us.freeandfair.corla.util.FileHelper;
+import us.freeandfair.corla.util.SparkHelper;
+
+/**
+ * The "ballot manifest upload" endpoint.
+ * 
+ * @author Daniel M. Zimmerman
+ * @version 0.0.1
+ */
+@SuppressWarnings({"PMD.AtLeastOneConstructor", "PMD.ExcessiveImports"})
+public class EstablishAuditBoard extends AbstractCountyDashboardEndpoint {
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public EndpointType endpointType() {
+    return EndpointType.POST;
+  }
+  
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public String endpointName() {
+    return "/audit-board";
+  }
+
+  /**
+   * This endpoint requires county authorization.
+   * 
+   * @return COUNTY
+   */
+  public AuthorizationType requiredAuthorization() {
+    return AuthorizationType.COUNTY;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public String endpoint(final Request the_request,
+                         final Response the_response) {
+    return "The audit board specified has been saved.";
+  }
+}
