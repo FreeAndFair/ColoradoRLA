2400eb8779ae1b438fdba289fcdc43fdc2f13e13
Joe Ranweiler <joe@lemma.co>
2017-08-14 01:56:15 -0700

    Add DOS dashboard refresh adapter.

---
+++ client/src/adapter/dosDashboardRefresh.ts
@@ -0,0 +1,22 @@
+import * as _ from 'lodash';
+
+
+const parseCountyStatus = (countyStatus: any) => {
+    const result: any = {};
+
+    _.forEach(countyStatus, (status, strId) => {
+        const id = parseInt(strId, 10);
+
+        result[id] = status;
+    });
+
+    return result;
+};
+
+export const parse = (data: any) => ({
+    auditStage: data.audit_stage,
+    countyStatus: parseCountyStatus(data.county_status),
+    handCountContests: data.hand_count_contests,
+    riskLimit: data.riskLimit,
+    seed: data.random_seed,
+});
