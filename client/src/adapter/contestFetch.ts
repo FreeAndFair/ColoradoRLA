d959f8df4439e7d9758dd721de7ae746629cd598
Joe Ranweiler <joe@lemma.co>
2017-08-14 02:15:25 -0700

    Implement DOS contest fetch adapter.

--- client/src/adapter/contestFetch.ts
+++ client/src/adapter/contestFetch.ts
@@ -1,3 +1,13 @@
-export const parse = (contests: any) => {
-    return contests;
+import * as _ from 'lodash';
+
+
+const parseContest = (c: any) => {
+    const result: any = _.omit(c, 'votes_allowed');
+
+    result.votesAllowed = c.votes_allowed;
+
+    return result;
 };
+
+
+export const parse = (contests: any) => contests.map(parseContest);
