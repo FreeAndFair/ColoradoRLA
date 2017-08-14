73cec826ccaa3a01ba28d2f0ff2758432454536c
Joe Ranweiler <joe@lemma.co>
2017-08-14 12:15:23 -0700

    Add county dashboard refresh adapter.

--- client/src/reducer/countyDashboardRefreshOk.ts
+++ client/src/reducer/countyDashboardRefreshOk.ts
@@ -1,8 +1,11 @@
+import { parse } from '../adapter/countyDashboardRefresh';
+
+
 export default (state: any, action: any) => {
     const nextState = { ...state };
 
-    const { data } = action;
-    nextState.county = { ...state.county, ...data };
+    const newState = parse(action.data, state);
+    nextState.county = { ...state.county, ...newState };
 
     return nextState;
 };
