71edd25dc52bc525fbe36c50773e04dd3f7c565f
Joe Ranweiler <joe@lemma.co>
2017-08-14 13:18:33 -0700

    Initialize more DOS dashboard state on login.

--- client/src/reducer/dosAuthOk.ts
+++ client/src/reducer/dosAuthOk.ts
@@ -1,6 +1,29 @@
+import * as _ from 'lodash';
+
+import counties from '../data/counties';
+
+
+type AuditStage
+    = 'PRE_AUDIT'
+    | 'AUDIT_READY_TO_START'
+    | 'AUDIT_ONGOING'
+    | 'AUDIT_COMPLETE'
+    | 'AUDIT_RESULTS_PUBLISHED';
+
+
+const initialCountyStatus = () => _.mapValues(counties, () => 'NO_DATA');
+
+const sosInitialState = (): any => ({
+    auditStage: 'PRE_AUDIT',
+    auditedContests: [],
+    countyStatus: initialCountyStatus(),
+    riskLimit: 0.05,
+});
+
+
 export default (state: any) => ({
     ...state,
     dashboard: 'sos',
     loggedIn: true,
-    sos: {},
+    sos: sosInitialState(),
 });
