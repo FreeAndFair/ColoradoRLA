431f1733558c233652e5279032daf949f61424fa
Joe Ranweiler <joe@lemma.co>
2017-08-15 09:52:26 -0700

    Initiate initial DOS data fetches on login.

--- client/src/component/login/LoginFormContainer.tsx
+++ client/src/component/login/LoginFormContainer.tsx
@@ -6,6 +6,8 @@ import LoginForm, { FormFields } from './LoginForm';
 
 import authCountyAdmin from '../../action/authCountyAdmin';
 import authStateAdmin from '../../action/authStateAdmin';
+import dosDashboardRefresh from '../../action/dosDashboardRefresh';
+import fetchContests from '../../action/fetchContests';
 
 
 export class LoginFormContainer extends React.Component<any, any> {
@@ -22,13 +24,22 @@ const mapDispatchToProps = (dispatch: Dispatch<any>) => {
     const b = bindActionCreators({
         authCountyAdmin,
         authStateAdmin,
+        dosDashboardRefresh,
+        fetchContests,
     }, dispatch);
 
     const submit = ({ dashboard, username, password }: any) => {
         switch (dashboard) {
-            case 'sos': return b.authStateAdmin(username, password);
-            case 'county': return b.authCountyAdmin(username, password);
-            default: return null;
+            case 'sos': {
+                b.authStateAdmin(username, password);
+                b.dosDashboardRefresh();
+                b.fetchContests();
+                break;
+            }
+            case 'county': {
+                b.authCountyAdmin(username, password);
+                break;
+            }
         }
     };
 
