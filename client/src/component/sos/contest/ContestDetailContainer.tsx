9aef943eabe48d860f78e492df1e91a69ae55d8e
Joe Ranweiler <joe@lemma.co>
2017-08-15 10:51:58 -0700

    Pass DOS state to contest detail page.

--- client/src/component/sos/contest/ContestDetailContainer.tsx
+++ client/src/component/sos/contest/ContestDetailContainer.tsx
@@ -1,21 +1,39 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators, Dispatch } from 'redux';
+
+import fetchContests from '../../../action/fetchContests';
 
 import ContestDetailPage from './ContestDetailPage';
 
 
 class ContestDetailContainer extends React.Component<any, any> {
     public render() {
+        const { contests, fetchContests } = this.props;
+
+        if (!contests) {
+            fetchContests();
+            return <div />;
+        }
+
         const { contestId } = this.props.match.params;
         const contest = this.props.contests[contestId];
+
+        if (!contest) {
+            // This might only ever be evidence of a bug.
+            fetchContests();
+            return <div />;
+        }
 
         return <ContestDetailPage contest={ contest } />;
     }
 }
 
-const mapStateToProps = ({ contests }: any) => ({ contests });
+const mapStateToProps = ({ sos }: any) => ({ contests: sos.contests });
 
-const mapDispatchToProps = (dispatch: any) => ({});
+const mapDispatchToProps = (dispatch: Dispatch<any>) => bindActionCreators({
+    fetchContests,
+}, dispatch);
 
 export default connect(
     mapStateToProps,
