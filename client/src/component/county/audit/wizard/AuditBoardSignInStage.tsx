6d3ea5d0a6197050a10c67e1cd9210b0000bcbd6
Joe Ranweiler <joe@lemma.co>
2017-08-16 12:37:03 -0700

    Don't render audit board sign-in form if already signed in.

--- client/src/component/county/audit/wizard/AuditBoardSignInStage.tsx
+++ client/src/component/county/audit/wizard/AuditBoardSignInStage.tsx
@@ -13,6 +13,44 @@ const AuditBoardSignInStage = (props: any) => {
         nextStage();
     };
 
+    if (auditBoard.length == 2) {
+        return (
+            <div>
+                <div>
+                    <h2>Audit Board Sign-in</h2>
+                    <div className='pt-card'>
+                        Enter the full names and party affiliations of each member of
+                        the Acme County Audit Board who will be conducting this audit
+                        today:
+                    </div>
+                </div>
+                <div className='pt-card'>
+                    <h4>Board Member 1:</h4>
+                    <div>
+                        Name: { auditBoard[0].firstName } { auditBoard[0].lastName }
+                    </div>
+                    <div>
+                        Political party: { auditBoard[0].party }
+                    </div>
+                </div>
+                <div className='pt-card'>
+                    <h4>Board Member 2:</h4>
+                    <div>
+                        Name: { auditBoard[1].firstName } { auditBoard[1].lastName }
+                    </div>
+                    <div>
+                        Political party: { auditBoard[1].party }
+                    </div>
+                </div>
+                <div>
+                </div>
+                <button className='pt-button pt-intent-primary' onClick={ submit }>
+                    Submit & Next
+                </button>
+            </div>
+        );
+    }
+
     return (
         <div>
             <div>
