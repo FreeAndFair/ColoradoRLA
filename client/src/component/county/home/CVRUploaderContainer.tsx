12e30af2e98f216724f924593a5904ab63c3082a
Joe Ranweiler <joe@lemma.co>
2017-08-15 18:20:01 -0700

    Add hash text input, integrate CVR export upload.

--- client/src/component/county/home/CVRUploaderContainer.tsx
+++ client/src/component/county/home/CVRUploaderContainer.tsx
@@ -1,27 +1,32 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators, Dispatch } from 'redux';
 
 import CVRUploader from './CVRUploader';
+
+import uploadCvrExport from '../../../action/uploadCvrExport';
 
 
 class CVRUploaderContainer extends React.Component<any, any> {
     public render() {
-        const onChange = (e: any) => {
-            const input = e.target;
-            const file = input.files[0];
-            // Trigger async file upload action here.
+        const { county, uploadCvrExport } = this.props;
+        const forms: any = {};
+
+        const upload = () => {
+            const { file, hash } = forms.cvrExportForm;
+
+            uploadCvrExport(county.id, file, hash);
         };
 
-        // This will change in app state upon successful file upload.
-        const fileName = 'No file uploaded.';
-
-        return <CVRUploader onChange={ onChange } fileName={ fileName } />;
+        return <CVRUploader upload={ upload } forms={ forms } />;
     }
 }
 
-const mapStateToProps = () => ({});
+const mapStateToProps = ({ county }: any) => ({ county });
 
-const mapDispatchToProps = (dispatch: any) => ({});
+const mapDispatchToProps = (dispatch: Dispatch<any>) => bindActionCreators({
+    uploadCvrExport,
+}, dispatch);
 
 export default connect(
     mapStateToProps,
