d8eef9ea0871534236acf902a478994279b6a485
Joe Ranweiler <joe@lemma.co>
2017-08-15 18:05:20 -0700

    Add hash text input, integrate ballot manifest upload.

--- client/src/component/county/home/BallotManifestUploaderContainer.tsx
+++ client/src/component/county/home/BallotManifestUploaderContainer.tsx
@@ -1,27 +1,32 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators, Dispatch } from 'redux';
 
 import BallotManifestUploader from './BallotManifestUploader';
+
+import uploadBallotManifest from '../../../action/uploadBallotManifest';
 
 
 class BallotManifestUploaderContainer extends React.Component<any, any> {
     public render() {
-        const onChange = (e: any) => {
-            const input = e.target;
-            const file = input.files[0];
-            // Trigger async file upload action here.
+        const { county, uploadBallotManifest } = this.props;
+        const forms: any = {};
+
+        const upload = () => {
+            const { file, hash } = forms.ballotManifestForm;
+
+            uploadBallotManifest(county.id, file, hash);
         };
 
-        // This will change in app state upon successful file upload.
-        const fileName = 'No file uploaded.';
-
-        return <BallotManifestUploader onChange={ onChange } fileName={ fileName } />;
+        return <BallotManifestUploader upload={ upload } forms={ forms } />;
     }
 }
 
-const mapStateToProps = () => ({});
+const mapStateToProps = ({ county }: any) => ({ county });
 
-const mapDispatchToProps = (dispatch: any) => ({});
+const mapDispatchToProps = (dispatch: Dispatch<any>) => bindActionCreators({
+    uploadBallotManifest,
+}, dispatch);
 
 export default connect(
     mapStateToProps,
