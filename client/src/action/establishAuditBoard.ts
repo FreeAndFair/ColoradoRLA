eac519ef0642dd3ec713b07de3759e2032e38e20
Joe Ranweiler <joe@lemma.co>
2017-08-16 12:14:27 -0700

    Fix typo in establish audit board actino type.

--- client/src/action/establishAuditBoard.ts
+++ client/src/action/establishAuditBoard.ts
@@ -10,10 +10,10 @@ import { format } from '../adapter/establishBoard';
 const url = endpoint('audit-board');
 
 const establishAuditBoard = createSubmitAction({
-    failType: 'ESABLISH_AUDIT_BOARD_FAIL',
-    networkFailType: 'ESABLISH_AUDIT_BOARD_NETWORK_FAIL',
-    okType: 'ESABLISH_AUDIT_BOARD_OK',
-    sendType: 'ESABLISH_AUDIT_BOARD_SEND',
+    failType: 'ESTABLISH_AUDIT_BOARD_FAIL',
+    networkFailType: 'ESTABLISH_AUDIT_BOARD_NETWORK_FAIL',
+    okType: 'ESTABLISH_AUDIT_BOARD_OK',
+    sendType: 'ESTABLISH_AUDIT_BOARD_SEND',
     url,
 });
 
