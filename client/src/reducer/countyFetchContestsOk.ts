c228a0d6dc9fc2d4f69c9778bf03565605537ffa
Joe Ranweiler <joe@lemma.co>
2017-08-16 15:54:13 -0700

    Fix parsing of county contest definitions in reducer.

--- client/src/reducer/countyFetchContestsOk.ts
+++ client/src/reducer/countyFetchContestsOk.ts
@@ -1,9 +1,37 @@
-import { merge } from 'lodash';
+import * as _ from 'lodash';
+const { merge } = _;
+
+
+const parseChoices = (names: any, descriptions: any) => {
+    return _.map(names, (name: any) => ({
+        name,
+        description: descriptions[name],
+    }));
+};
+
+const parseDef = (c: any): any => {
+    const { description, id, name } = c;
+
+    const choices = parseChoices(c.choice_names, c.choice_descriptions);
+
+    return { id, choices, description, name };
+};
+
+const parse = (data: any, state: any) => {
+    const defs: any = {};
+
+    data.forEach((c: any) => {
+        defs[c.id] = parseDef(c);
+    });
+
+    return defs;
+}
+
 
 export default (state: any, action: any) => {
     const county = merge({}, state.county);
 
-    county.contests = merge({}, action.data);
+    county.contestDefs = merge({}, parse(action.data, state));
 
     return merge({}, state, { county });
 };
