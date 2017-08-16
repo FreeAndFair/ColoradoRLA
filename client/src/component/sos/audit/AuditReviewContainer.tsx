d3e5d9a52650968510c8cf13d5c39bba2fe4e273
Joe Ranweiler <joe@lemma.co>
2017-08-16 12:55:08 -0700

    Add publish ballots to audit action, dispatch from DOS dashboard.

--- client/src/component/sos/audit/AuditReviewContainer.tsx
+++ client/src/component/sos/audit/AuditReviewContainer.tsx
@@ -1,21 +1,31 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators, Dispatch } from 'redux';
 
 import AuditReviewPage from './AuditReviewPage';
+
+import publishBallotsToAudit from '../../../action/publishBallotsToAudit';
 
 
 class AuditBallotListContainer extends React.Component<any, any> {
     public render() {
-        const back = () => this.props.history.push('/sos/audit/seed');
-        const saveAndDone = () => this.props.history.push('/sos');
+        const { history, publishBallotsToAudit } = this.props;
 
-        return <AuditReviewPage { ...{ back, saveAndDone} } />;
+        const props = {
+            back: () => history.push('/sos/audit/seed'),
+            publishBallotsToAudit,
+            saveAndDone: () => history.push('/sos'),
+        };
+
+        return <AuditReviewPage { ...props } />;
     }
 }
 
 const mapStateToProps = () => ({});
 
-const mapDispatchToProps = (dispatch: any) => ({});
+const mapDispatchToProps = (dispatch: Dispatch<any>) => bindActionCreators({
+    publishBallotsToAudit,
+}, dispatch);
 
 export default connect(
     mapStateToProps,
