69c151723bd243b6789a031fd6ec9e2d5c1894f0
Joe Ranweiler <joe@lemma.co>
2017-08-16 20:50:17 -0700

    SQUASH: fix acvr reducer

--- client/src/reducer/updateAcvrForm.ts
+++ client/src/reducer/updateAcvrForm.ts
@@ -2,7 +2,7 @@ import * as _ from 'lodash';
 
 
 export default (state: any, action: any) => {
-    const nextState = _.merge({} state);
+    const nextState: any = _.merge({}, state);
 
     const {
         ballotId,
@@ -20,7 +20,7 @@ export default (state: any, action: any) => {
         },
     };
 
-    const acvrs[ballotId] = acvr;
+    const acvrs = { [ballotId]: acvr };
     nextState.county.acvrs = _.merge({}, state.county.acvrs, acvrs);
 
     return nextState;
