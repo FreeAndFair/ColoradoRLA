7efde97f58a88cc9df9df998f23ff3dca4055bfa
Joe Ranweiler <joe@lemma.co>
2017-08-20 12:16:19 -0700

    Use new county login action in login form container.

--- client/src/component/login/LoginFormContainer.tsx
+++ client/src/component/login/LoginFormContainer.tsx
@@ -1,45 +1,29 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
-import { bindActionCreators, Dispatch } from 'redux';
 
 import LoginForm, { FormFields } from './LoginForm';
 
-import authCountyAdmin from '../../action/authCountyAdmin';
+import countyLogin from '../../action/countyLogin';
 import dosLogin from '../../action/dosLogin';
 
 
+const submit = ({ dashboard, username, password }: any) => {
+    switch (dashboard) {
+        case 'sos': {
+            dosLogin(username, password);
+            break;
+        }
+        case 'county': {
+            countyLogin(username, password);
+            break;
+        }
+    }
+};
+
 export class LoginFormContainer extends React.Component<any, any> {
     public render() {
-        const { submit } = this.props;
-
         return <LoginForm submit={ submit } />;
     }
 }
 
-const mapStateToProps = () => ({});
-
-const mapDispatchToProps = (dispatch: Dispatch<any>) => {
-    const b = bindActionCreators({
-        authCountyAdmin,
-    }, dispatch);
-
-    const submit = ({ dashboard, username, password }: any) => {
-        switch (dashboard) {
-            case 'sos': {
-                dosLogin(username, password);
-                break;
-            }
-            case 'county': {
-                b.authCountyAdmin(username, password);
-                break;
-            }
-        }
-    };
-
-    return { submit };
-};
-
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps,
-)(LoginFormContainer);
+export default connect()(LoginFormContainer);
