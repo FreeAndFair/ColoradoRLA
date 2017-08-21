360eb217caa5a6e237e74dfa2824fb62ce37e7ce
Joe Ranweiler <joe@lemma.co>
2017-08-20 23:57:39 -0700

    Notify user of ACVR upload status.

--- client/src/saga/fileUploadSaga.ts
+++ client/src/saga/fileUploadSaga.ts
@@ -27,6 +27,18 @@ function* uploadCvrExportNetworkFail(): any {
     notice.danger('Network error: failed to upload ballot manifest.');
 }
 
+function* uploadAcvrOk(): any {
+    notice.ok('Uploaded ACVR.');
+}
+
+function* uploadAcvrFail(): any {
+    notice.danger('Failed to upload ACVR.');
+}
+
+function* uploadAcvrNetworkFail(): any {
+    notice.danger('Network error: failed to upload ACVR.');
+}
+
 export default function* fileUploadSaga() {
     yield takeEvery('UPLOAD_BALLOT_MANIFEST_OK', uploadBallotManifestOk);
     yield takeEvery('UPLOAD_BALLOT_MANIFEST_FAIL', uploadBallotManifestFail);
@@ -36,4 +48,8 @@ export default function* fileUploadSaga() {
     yield takeEvery('UPLOAD_CVR_EXPORT_OK', uploadCvrExportOk);
     yield takeEvery('UPLOAD_CVR_EXPORT_FAIL', uploadCvrExportFail);
     yield takeEvery('UPLOAD_CVR_EXPORT_NETWORK_FAIL', uploadCvrExportNetworkFail);
+
+    yield takeEvery('UPLOAD_ACVR_OK', uploadAcvrOk);
+    yield takeEvery('UPLOAD_ACVR_FAIL', uploadAcvrFail);
+    yield takeEvery('UPLOAD_ACVR_NETWORK_FAIL', uploadAcvrNetworkFail);
 }
