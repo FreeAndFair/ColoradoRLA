ee7c6d683138c7b3e4bd892729ebd54f925ef61a
Joe Ranweiler <joe@lemma.co>
2017-08-21 13:31:20 -0700

    Add types, return async func from next `createFetchAction`.

--- client/src/action/next/createFetchAction.ts
+++ client/src/action/next/createFetchAction.ts
@@ -1,33 +1,49 @@
 import action from '..';
 
 
-const createFetchAction = ({
-    failType,
-    networkFailType,
-    okType,
-    sendType,
-    url,
-}: any) => () => {
-    action(sendType);
+interface CreateFetchConfig {
+    failType: string;
+    networkFailType: string;
+    okType: string;
+    sendType: string;
+    url: string;
+}
 
-    const init: any = {
-        credentials: 'include',
-        method: 'get',
-    };
 
-    return fetch(url, init)
-        .then(r => {
+function createFetchAction(config: CreateFetchConfig) {
+    const {
+        failType,
+        networkFailType,
+        okType,
+        sendType,
+        url,
+    } = config;
+
+    async function fetchAction() {
+        action(sendType);
+
+        const init: any = {
+            credentials: 'include',
+            method: 'get',
+        };
+
+        try {
+            const r = await fetch(url, init);
+
             if (!r.ok) {
                 action(failType);
-                return;
             }
 
-            r.json().then(data => {
-                action(okType, data);
-            });
-        })
-        .catch(() => action(networkFailType));
-};
+            const data = await r.json();
+
+            action(okType, data);
+        } catch {
+            action(networkFailType);
+        }
+    }
+
+    return fetchAction;
+}
 
 
 export default createFetchAction;
