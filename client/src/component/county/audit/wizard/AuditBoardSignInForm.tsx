a37505b2330abc03f9adcdc8638d092eb95871c4
Joe Ranweiler <joe@lemma.co>
2017-08-22 15:46:22 -0700

    Add validation to audit board sign-in form.

--- client/src/component/county/audit/wizard/AuditBoardSignInForm.tsx
+++ client/src/component/county/audit/wizard/AuditBoardSignInForm.tsx
@@ -3,90 +3,60 @@ import * as React from 'react';
 import { EditableText, Radio, RadioGroup } from '@blueprintjs/core';
 
 
-class AuditBoardSignInForm extends React.Component<any, any> {
-    public state = {
-        firstName: '',
-        lastName: '',
-        party: '',
-    };
+const AuditBoardSignInForm = (props: any) => {
+    const {
+        elector,
+        onFirstNameChange,
+        onLastNameChange,
+        onPartyChange,
+    } = props;
 
-    public render() {
-        const { boardMemberIndex, forms } = this.props;
+    const { firstName, lastName, party } = elector;
 
-        forms.auditBoard[boardMemberIndex] = this.state;
-
-        const { firstName, lastName, party } = this.state;
-
-        return (
-            <div>
-                <h3>Audit Board Member</h3>
-                <div className='pt-card'>
-                    <label>
-                        First Name:
-                        <EditableText
-                            className='pt-input'
-                            value={ firstName }
-                            onChange={ this.onFirstNameChange } />
-                        Last Name:
-                        <EditableText
-                            className='pt-input'
-                            value={ lastName }
-                            onChange={ this.onLastNameChange } />
-                    </label>
-                </div>
-                <div className='pt-card'>
-                    <RadioGroup
-                        label='Party Affiliation'
-                        onChange={ this.onPartyChange }
-                        selectedValue={ party }
-                    >
-                        <Radio
-                            label='Democratic Party'
-                            value='Democratic Party'
-                        />
-                        <Radio
-                            label='Republican Party'
-                            value='Republican Party'
-                        />
-                        <Radio
-                            label='Other Party'
-                            value='Other Party'
-                        />
-                        <Radio
-                            label='Unaffiliated'
-                            value='Unaffiliated'
-                        />
-                    </RadioGroup>
-                </div>
+    return (
+        <div>
+            <h3>Audit Board Member</h3>
+            <div className='pt-card'>
+                <label>
+                    First Name:
+                    <EditableText
+                        className='pt-input'
+                        value={ firstName }
+                        onChange={ onFirstNameChange } />
+                    Last Name:
+                    <EditableText
+                        className='pt-input'
+                        value={ lastName }
+                        onChange={ onLastNameChange } />
+                </label>
             </div>
-        );
-    }
-
-    private onFirstNameChange = (name: string) => {
-        const s = { ...this.state };
-
-        s.firstName = name
-
-        this.setState(s);
-    }
-
-    private onLastNameChange = (name: string) => {
-        const s = { ...this.state };
-
-        s.lastName = name;
-
-        this.setState(s);
-    }
-
-    private onPartyChange = (e: any) => {
-        const s = { ...this.state };
-
-        const party = e.target.value;
-        s.party = party;
-
-        this.setState(s);
-    }
-}
+            <div className='pt-card'>
+                <RadioGroup
+                    label='Party Affiliation'
+                    onChange={ onPartyChange }
+                    selectedValue={ party }
+                >
+                    <Radio
+                        label='Democratic Party'
+                        value='Democratic Party'
+                    />
+                    <Radio
+                        label='Republican Party'
+                        value='Republican Party'
+                    />
+                    <Radio
+                        label='Other Party'
+                        value='Other Party'
+                    />
+                    <Radio
+                        label='Unaffiliated'
+                        value='Unaffiliated'
+                    />
+                </RadioGroup>
+            </div>
+        </div>
+    );
+};
 
 
 export default AuditBoardSignInForm;
--- client/src/component/county/audit/wizard/AuditBoardSignInStage.tsx
+++ client/src/component/county/audit/wizard/AuditBoardSignInStage.tsx
@@ -5,21 +5,85 @@ import AuditBoardSignInForm from './AuditBoardSignInForm';
 import establishAuditBoard from '../../../../action/establishAuditBoard';
 
 
-const AuditBoardSignInStage = (props: any) => {
-    const { auditBoard, county, nextStage } = props;
+function validateElector(elector: any) {
+    return elector.firstName
+        && elector.lastName
+        && elector.party;
+}
 
-    const forms: any = { auditBoard: [] };
+function validateForm(auditBoard: any) {
+    return validateElector(auditBoard[0])
+        && validateElector(auditBoard[1]) ;
+}
 
-    if (!auditBoard) {
-        return <div />;
-    }
-
-    const submit = () => {
-        establishAuditBoard(forms.auditBoard);
-        nextStage();
+class AuditBoardSignInStage extends React.Component<any, any> {
+    public state = {
+        form: [
+            {
+                firstName: '',
+                lastName: '',
+                party: '',
+            },
+            {
+                firstName: '',
+                lastName: '',
+                party: '',
+            },
+        ],
     };
 
-    if (auditBoard.length === 2) {
+    public render() {
+        const { auditBoard, county, nextStage } = this.props;
+
+        if (!auditBoard) {
+            return <div />;
+        }
+
+        const submit = () => {
+            establishAuditBoard(this.state.form);
+            nextStage();
+        };
+
+        if (auditBoard.length === 2) {
+            return (
+                <div>
+                    <div>
+                        <h2>Audit Board Sign-in</h2>
+                        <div className='pt-card'>
+                            Enter the full names and party affiliations of each member of
+                            the Acme County Audit Board who will be conducting this audit
+                            today:
+                        </div>
+                    </div>
+                    <div className='pt-card'>
+                        <h4>Board Member 1:</h4>
+                        <div>
+                            Name: { auditBoard[0].firstName } { auditBoard[0].lastName }
+                        </div>
+                        <div>
+                            Political party: { auditBoard[0].party }
+                        </div>
+                    </div>
+                    <div className='pt-card'>
+                        <h4>Board Member 2:</h4>
+                        <div>
+                            Name: { auditBoard[1].firstName } { auditBoard[1].lastName }
+                        </div>
+                        <div>
+                            Political party: { auditBoard[1].party }
+                        </div>
+                    </div>
+                    <div>
+                    </div>
+                    <button className='pt-button pt-intent-primary' onClick={ nextStage }>
+                        Submit & Next
+                    </button>
+                </div>
+            );
+        }
+
+        const disableButton = !validateForm(this.state.form);
+
         return (
             <div>
                 <div>
@@ -30,57 +94,53 @@ const AuditBoardSignInStage = (props: any) => {
                         today:
                     </div>
                 </div>
-                <div className='pt-card'>
-                    <h4>Board Member 1:</h4>
-                    <div>
-                        Name: { auditBoard[0].firstName } { auditBoard[0].lastName }
-                    </div>
-                    <div>
-                        Political party: { auditBoard[0].party }
-                    </div>
-                </div>
-                <div className='pt-card'>
-                    <h4>Board Member 2:</h4>
-                    <div>
-                        Name: { auditBoard[1].firstName } { auditBoard[1].lastName }
-                    </div>
-                    <div>
-                        Political party: { auditBoard[1].party }
-                    </div>
-                </div>
-                <div>
-                </div>
-                <button className='pt-button pt-intent-primary' onClick={ nextStage }>
+                <AuditBoardSignInForm
+                    elector={ this.state.form[0] }
+                    onFirstNameChange={ this.onFirstNameChange(0) }
+                    onLastNameChange={ this.onLastNameChange(0) }
+                    onPartyChange={ this.onPartyChange(0) }
+                />
+                <AuditBoardSignInForm
+                    elector={ this.state.form[1] }
+                    onFirstNameChange={ this.onFirstNameChange(1) }
+                    onLastNameChange={ this.onLastNameChange(1) }
+                    onPartyChange={ this.onPartyChange(1) }
+                />
+                <button
+                    className='pt-button pt-intent-primary'
+                    disabled={ disableButton }
+                    onClick={ submit }>
                     Submit & Next
                 </button>
             </div>
         );
     }
 
-    return (
-        <div>
-            <div>
-                <h2>Audit Board Sign-in</h2>
-                <div className='pt-card'>
-                    Enter the full names and party affiliations of each member of
-                    the Acme County Audit Board who will be conducting this audit
-                    today:
-                </div>
-            </div>
-            <AuditBoardSignInForm
-                boardMemberIndex={ 0 }
-                forms={ forms }
-            />
-            <AuditBoardSignInForm
-                boardMemberIndex={ 1 }
-                forms={ forms }
-            />
-            <button className='pt-button pt-intent-primary' onClick={ submit }>
-                Submit & Next
-            </button>
-        </div>
-    );
-};
+    private onFirstNameChange = (index: number) => (name: string) => {
+        const s = { ...this.state };
+
+        s.form[index].firstName = name;
+
+        this.setState(s);
+    }
+
+    private onLastNameChange = (index: number) => (name: string) => {
+        const s = { ...this.state };
+
+        s.form[index].lastName = name;
+
+        this.setState(s);
+    }
+
+    private onPartyChange = (index: number) => (e: any) => {
+        const s = { ...this.state };
+
+        const party = e.target.value;
+        s.form[index].party = party;
+
+        this.setState(s);
+    }
+}
 
 
 export default AuditBoardSignInStage;
