cff04b00ee25a6ff3cb55d7d419ef3f6e2f9946e
Joe Ranweiler <joe@lemma.co>
2017-08-23 12:14:15 -0700

    Add fetch audit board ASM state action.

---
+++ client/src/action/fetchAuditBoardAsmState.ts
@@ -0,0 +1,15 @@
+import { endpoint } from '../config';
+
+import createFetchAction from './createFetchAction';
+
+
+const url = endpoint('audit-board-asm-state');
+
+
+export default createFetchAction({
+    failType: 'FETCH_AUDIT_BOARD_ASM_STATE_FAIL',
+    networkFailType: 'FETCH_AUDIT_BOARD_ASM_STATE_NETWORK_FAIL',
+    okType: 'FETCH_AUDIT_BOARD_ASM_STATE_OK',
+    sendType: 'FETCH_AUDIT_BOARD_ASM_STATE_SEND',
+    url,
+});
