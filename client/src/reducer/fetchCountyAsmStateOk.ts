1a0d166b17844062b0ced76566be4e868f0e72be
Joe Ranweiler <joe@lemma.co>
2017-08-23 15:03:41 -0700

    Parse data when reducing fetch county ASM ok.

--- client/src/reducer/fetchCountyAsmStateOk.ts
+++ client/src/reducer/fetchCountyAsmStateOk.ts
@@ -1,5 +1,18 @@
+import * as _ from 'lodash';
+
+
+function parse(data: any) {
+    return {
+        currentState: data.current_state,
+        enabledUiEvents: data.enabled_ui_events,
+    };
+}
+
+
 export default (state: any, action: any) => {
     const nextState = { ...state };
+
+    nextState.county.asm.county = parse(action.data);
 
     return nextState;
 };
