89928d6b320da1b565bcedc7bb617c80a94bbd2b
Joe Ranweiler <joe@lemma.co>
2017-08-23 15:59:25 -0700

    Render county audit container redirect based on ASM state.

--- client/src/component/county/audit/CountyAuditContainer.tsx
+++ client/src/component/county/audit/CountyAuditContainer.tsx
@@ -1,16 +1,28 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 
 import CountyAuditPage from './CountyAuditPage';
+
+import notice from '../../../notice';
 
 
 class CountyAuditContainer extends React.Component<any, any> {
     public render() {
+        if (!this.props.auditInProgress) {
+            notice.danger('Not ready to begin audit.');
+            return <Redirect to={ '/county' } />;
+        }
+
         return <CountyAuditPage />;
     }
 }
 
-const mapStateToProps = () => ({});
+const mapStateToProps = (state: any) => {
+    return {
+        auditInProgress: state.county.asm.auditBoard.currentState === 'AUDIT_IN_PROGRESS',
+    };
+};
 
 const mapDispatchToProps = (dispatch: any) => ({});
 
