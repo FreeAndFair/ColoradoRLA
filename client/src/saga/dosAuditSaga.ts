5b85b2181d9f44d6a825fbee70c217a45ba7fe25
Joe Ranweiler <joe@lemma.co>
2017-08-24 13:18:22 -0700

    Notify DOS user on failure select contests for audit.

--- client/src/saga/dosAuditSaga.ts
+++ client/src/saga/dosAuditSaga.ts
@@ -3,6 +3,15 @@ import { takeLatest } from 'redux-saga/effects';
 import notice from '../notice';
 
 
+function* selectContestsForAuditFail(): IterableIterator<void> {
+    notice.danger('Unable to select contests for audit.');
+    notice.danger('Please verify that you selected at least one contest for audit.');
+}
+
+function* selectContestsForAuditNetworkFail(): IterableIterator<void> {
+    notice.danger('Unable to select contests for audit: network failure.');
+}
+
 function* selectContestsForAuditOk(): IterableIterator<void> {
     notice.ok('Contests for audit are now selected.');
 }
@@ -25,6 +34,8 @@ function* uploadRandomSeedOk(): IterableIterator<void> {
 
 
 export default function* dosLoginSaga() {
+    yield takeLatest('SELECT_CONTESTS_FOR_AUDIT_FAIL', selectContestsForAuditFail);
+    yield takeLatest('SELECT_CONTESTS_FOR_AUDIT_NETWORK_FAIL', selectContestsForAuditNetworkFail);
     yield takeLatest('SELECT_CONTESTS_FOR_AUDIT_OK', selectContestsForAuditOk);
 
     yield takeLatest('SET_RISK_LIMIT_FAIL', setRiskLimitFail);
