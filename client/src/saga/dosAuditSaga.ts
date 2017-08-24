abfa9ea7a6de21acb194d1d958915ceeeeee934a
Joe Ranweiler <joe@lemma.co>
2017-08-24 13:14:20 -0700

    Notify DOS user on failure setting risk limit.

--- client/src/saga/dosAuditSaga.ts
+++ client/src/saga/dosAuditSaga.ts
@@ -11,13 +11,25 @@ function* setRiskLimitOk(): IterableIterator<void> {
     notice.ok('Comparison audit risk limit is now set.');
 }
 
+function* setRiskLimitFail(): IterableIterator<void> {
+    notice.danger('Unable to set risk limit.');
+}
+
+function* setRiskLimitNetworkFail(): IterableIterator<void> {
+    notice.danger('Unable to set risk limit: network failure.');
+}
+
 function* uploadRandomSeedOk(): IterableIterator<void> {
-    notice.ok('Random number generator seed is now set.');
+    notice.danger('Random number generator seed is now set.');
 }
 
 
 export default function* dosLoginSaga() {
     yield takeLatest('SELECT_CONTESTS_FOR_AUDIT_OK', selectContestsForAuditOk);
+
+    yield takeLatest('SET_RISK_LIMIT_FAIL', setRiskLimitFail);
+    yield takeLatest('SET_RISK_LIMIT_NETWORK_FAIL', setRiskLimitNetworkFail);
     yield takeLatest('SET_RISK_LIMIT_OK', setRiskLimitOk);
+
     yield takeLatest('UPLOAD_RANDOM_SEED_OK', uploadRandomSeedOk);
 }
