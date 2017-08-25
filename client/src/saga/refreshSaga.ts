8530262ae3b479b6686154d8d19174e081b55168
Joe Ranweiler <joe@lemma.co>
2017-08-24 17:53:05 -0700

    Add refresh saga.
    This refreshes a given dashboard based on certain actions which mutate
    the server state, and where we'd expect to see an immediate UI update.
    This requires triggering a server poll, rather than waiting on the
    next scheduled poll.

---
+++ client/src/saga/refreshSaga.ts
@@ -0,0 +1,44 @@
+import {
+    put,
+    takeLatest,
+} from 'redux-saga/effects';
+
+import countyDashboardRefresh from '../action/countyDashboardRefresh';
+import dosDashboardRefresh from '../action/dosDashboardRefresh';
+import dosFetchAsmState from '../action/dosFetchAsmState';
+import dosFetchContests from '../action/dosFetchContests';
+import fetchAuditBoardAsmState from '../action/fetchAuditBoardAsmState';
+import fetchCountyAsmState from '../action/fetchCountyAsmState';
+
+
+function countyRefresh() {
+    countyDashboardRefresh();
+    fetchAuditBoardAsmState();
+    fetchCountyAsmState();
+}
+
+function dosRefresh() {
+    dosDashboardRefresh();
+    dosFetchAsmState();
+    dosFetchContests();
+}
+
+
+export default function* dosLoginSaga() {
+    const countyRefreshActions = [
+        'ESTABLISH_AUDIT_BOARD_OK',
+        'UPLOAD_ACVR_OK',
+        'UPDATE_ACVR_FORM',
+        'UPLOAD_BALLOT_MANIFEST_OK',
+        'UPLOAD_CVR_EXPORT_OK',
+    ];
+    yield takeLatest(countyRefreshActions, countyRefresh);
+
+    const dosRefreshActions = [
+        'PUBLISH_BALLOTS_TO_AUDIT_OK',
+        'SELECT_CONTESTS_FOR_AUDIT_OK',
+        'SET_RISK_LIMIT_OK',
+        'UPLOAD_RANDOM_SEED_OK',
+    ];
+    yield takeLatest(dosRefreshActions, dosRefresh);
+}
