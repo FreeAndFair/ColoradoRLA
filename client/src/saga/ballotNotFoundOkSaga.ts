31067b2cd46806eb5e48d0777221a387062e1d5a
Joe Ranweiler <joe@lemma.co>
2017-08-24 18:14:26 -0700

    Give county user more feedback when ballot recorded as not found.

--- client/src/saga/ballotNotFoundOkSaga.ts
+++ client/src/saga/ballotNotFoundOkSaga.ts
@@ -1,9 +1,12 @@
 import { takeLatest } from 'redux-saga/effects';
 
+import notice from '../notice';
+
 import countyDashboardRefresh from '../action/countyDashboardRefresh';
 
 
 function* ballotNotFoundOk(): any {
+    notice.ok('Previous ballot recorded as Not Found.');
     countyDashboardRefresh();
 }
 
