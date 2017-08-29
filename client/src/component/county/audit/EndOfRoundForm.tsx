b62e080085447df2a999f1ee35f039dbb457acb8
Joe Ranweiler <joe@lemma.co>
2017-08-29 14:08:00 -0700

    Outline end of round page layout and instructions.

---
+++ client/src/component/county/audit/EndOfRoundForm.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+
+class EndOfRoundForm extends React.Component<any, any> {
+    public render() {
+        return (
+            <div className='pt-card'>
+                End of round form.
+            </div>
+        );
+    }
+}
+
+
+export default EndOfRoundForm;
