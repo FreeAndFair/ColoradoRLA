89015ceaf1457c1a7e5eca254f7122bb0cdfcca3
Joe Ranweiler <joe@lemma.co>
2017-08-28 21:25:30 -0700

    Log out if another user logs in with the same browser context.

--- client/src/saga/sessionSaga.ts
+++ client/src/saga/sessionSaga.ts
@@ -1,7 +1,19 @@
-import { takeLatest } from 'redux-saga/effects';
+import { select, takeLatest } from 'redux-saga/effects';
 
 import * as cookies from 'js-cookie';
 
+
+function* checkSession(): IterableIterator<any> {
+    const { session } = yield select();
+
+    if (session) {
+        const currentSession = cookies.get('JSESSIONID');
+
+        if (session !== currentSession) {
+            window.location.replace('/login');
+        }
+    }
+}
 
 function* clearSession(): IterableIterator<void> {
     cookies.remove('JSESSIONID');
@@ -9,6 +21,9 @@ function* clearSession(): IterableIterator<void> {
 
 
 export default function* sessionSaga() {
+    const anyButLogout = (a: any) => a.type !== 'LOGOUT';
+    yield takeLatest(anyButLogout, checkSession);
+
     const clearSessionActions = [
         'COUNTY_LOGIN_SEND',
         'DOS_LOGIN_SEND',
