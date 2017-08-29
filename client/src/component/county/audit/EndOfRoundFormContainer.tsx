15f81956093fce33664ecbc3ea13417747fafbd1
Joe Ranweiler <joe@lemma.co>
2017-08-29 14:40:12 -0700

    Pass county info to end of round form.

--- client/src/component/county/audit/EndOfRoundFormContainer.tsx
+++ client/src/component/county/audit/EndOfRoundFormContainer.tsx
@@ -3,14 +3,26 @@ import { connect } from 'react-redux';
 
 import EndOfRoundForm from './EndOfRoundForm';
 
+import countyInfo from '../../../selector/county/countyInfo';
+
 
 class EndOfRoundFormContainer extends React.Component<any, any> {
     public render() {
-        return <EndOfRoundForm />;
+        const { countyInfo } = this.props;
+
+        const props = {
+            countyInfo,
+        };
+
+        return <EndOfRoundForm {...props} />;
     }
 }
 
-const mapStateToProps = (state: any) => ({});
+const mapStateToProps = (state: any) => {
+    return {
+        countyInfo: countyInfo(state),
+    };
+};
 
 
 export default connect(mapStateToProps)(EndOfRoundFormContainer);
