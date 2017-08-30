81dda07a9d0f126b969d0937c592dac093075766
Joe Ranweiler <joe@lemma.co>
2017-08-30 07:19:54 -0700

    Allow accessing county audit page if waiting for round sign-off.

--- client/src/selector/county/canAudit.ts
+++ client/src/selector/county/canAudit.ts
@@ -2,7 +2,8 @@ function canAudit(state: any) {
     const { county } = state;
     const { asm } = county;
 
-    return asm.auditBoard.currentState === 'AUDIT_IN_PROGRESS'
+    return (asm.auditBoard.currentState === 'AUDIT_IN_PROGRESS'
+            || asm.auditBoard.currentState === 'WAITING_FOR_ROUND_SIGN_OFF')
         && asm.county.currentState === 'COUNTY_AUDIT_UNDERWAY';
 }
 
