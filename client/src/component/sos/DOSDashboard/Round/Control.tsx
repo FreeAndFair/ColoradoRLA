b9a07cdc0778556b3f82f6911357ca00620367b3
Joe Ranweiler <joe@lemma.co>
2017-08-29 21:11:51 -0700

    Stub start next round form UI.

--- client/src/component/sos/DOSDashboard/Round/Control.tsx
+++ client/src/component/sos/DOSDashboard/Round/Control.tsx
@@ -3,8 +3,15 @@ import * as React from 'react';
 
 const Control = (props: any) => {
     return (
-        <div>
-            Round control
+        <div className='pt-card'>
+            <h4>Start next round</h4>
+            <div className='pt-card'>
+                Round [Round #N] has completed.
+                Start round [Round #N+1]?
+            </div>
+            <button className='pt-button pt-intent-primary'>
+                Start Round
+            </button>
         </div>
     );
 };
