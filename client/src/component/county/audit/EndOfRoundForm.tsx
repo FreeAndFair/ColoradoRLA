eb74813fba77f74f6852fecf23eba837f509c222
Joe Ranweiler <joe@lemma.co>
2017-08-29 18:28:34 -0700

    Add editable name fields to audit round sign-off form.

--- client/src/component/county/audit/EndOfRoundForm.tsx
+++ client/src/component/county/audit/EndOfRoundForm.tsx
@@ -1,10 +1,70 @@
 import * as React from 'react';
 
+import { EditableText } from '@blueprintjs/core';
 
-const EndOfRoundForm = (props: any) => {
+
+const ElectorFormField = (props: any) => {
+    const {
+        elector,
+        index,
+        onFirstNameChange,
+        onLastNameChange,
+        onTextConfirm,
+    } = props;
+
+    const { firstName, lastName } = elector;
+
     return (
         <div className='pt-card'>
-            End of round form.
+            <h3>Audit Board Member #{index + 1}</h3>
+            <div className='pt-card'>
+                <label>
+                    <span>
+                        First Name:
+                        <EditableText
+                            className='pt-input'
+                            value={ firstName }
+                            onChange={ onFirstNameChange }
+                            onConfirm={ onTextConfirm }
+                        />
+                    </span>
+                    <span>
+                        Last Name:
+                        <EditableText
+                            className='pt-input'
+                            value={ lastName }
+                            onChange={ onLastNameChange }
+                            onConfirm={ onTextConfirm }
+                        />
+                    </span>
+                </label>
+            </div>
+        </div>
+    );
+};
+
+const EndOfRoundForm = (props: any) => {
+    const {
+        form,
+        onFirstNameChange,
+        onLastNameChange,
+        onTextConfirm,
+    } = props;
+
+    const createFormField = (index: number) => (
+        <ElectorFormField
+            index={ index }
+            elector={ form[index] }
+            onFirstNameChange={ onFirstNameChange(index) }
+            onLastNameChange={ onLastNameChange(index) }
+            onTextConfirm={ onTextConfirm }
+        />
+    );
+
+    return (
+        <div>
+            { createFormField(0) }
+            { createFormField(1) }
         </div>
     );
 };
--- client/src/component/county/audit/EndOfRoundFormContainer.tsx
+++ client/src/component/county/audit/EndOfRoundFormContainer.tsx
@@ -7,14 +7,59 @@ import countyInfo from '../../../selector/county/countyInfo';
 
 
 class EndOfRoundFormContainer extends React.Component<any, any> {
+    public state = {
+        form: [
+            {
+                firstName: '',
+                lastName: '',
+            },
+            {
+                firstName: '',
+                lastName: '',
+            },
+        ],
+    };
+
     public render() {
         const { countyInfo } = this.props;
 
         const props = {
             countyInfo,
+            form: this.state.form,
+            onFirstNameChange: this.onFirstNameChange,
+            onLastNameChange: this.onLastNameChange,
+            onTextConfirm: this.onTextConfirm,
         };
 
         return <EndOfRoundForm {...props} />;
+    }
+
+    private onTextConfirm = () => {
+        const s = { ...this.state };
+
+        s.form[0].firstName = s.form[0].firstName.trim();
+        s.form[0].lastName = s.form[0].lastName.trim();
+
+        s.form[1].firstName = s.form[1].firstName.trim();
+        s.form[1].lastName = s.form[1].lastName.trim();
+
+        this.setState(s);
+    }
+
+    private onFirstNameChange = (index: number) => (name: string) => {
+        const s = { ...this.state };
+
+        s.form[index].firstName = name;
+
+        this.setState(s);
+    }
+
+    private onLastNameChange = (index: number) => (name: string) => {
+        const s = { ...this.state };
+
+        s.form[index].lastName = name;
+
+        this.setState(s);
     }
 }
 
