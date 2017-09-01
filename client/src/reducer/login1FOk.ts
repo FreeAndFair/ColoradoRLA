dd3e769e7f9a0bce10e54eaa49a53b8f2f947343
Joe Ranweiler <joe@lemma.co>
2017-08-31 23:33:52 -0700

    Parse 2FA challenge in reducer.

--- client/src/reducer/login1FOk.ts
+++ client/src/reducer/login1FOk.ts
@@ -1,9 +1,22 @@
+function parseBox(box: string): string[] {
+    return box.substring(1, box.length - 1).split(',');
+}
+
+function parseChallenge(challenge: string): string[][] {
+    const boxes = challenge.split(' ');
+
+    return boxes.map(parseBox);
+}
+
+
 function parse(data: any) {
     const dashboard = data.role === 'STATE'
         ? 'sos'
         : 'county';
 
-    const loginChallenge: any = null;
+    const loginChallenge = data.challenge
+        ? parseChallenge(data.challenge)
+        : null;
 
     return { dashboard, loginChallenge };
 }
