6dc7cfd80b5030afa8e09a2a76c69462827c5847
Joe Ranweiler <joe@lemma.co>
2017-09-01 04:32:49 -0700

    Add file fetch action creator.

---
+++ client/src/action/createFileFetchAction.ts
@@ -0,0 +1,59 @@
+import action from '.';
+
+
+interface CreateFetchConfig {
+    failType: string;
+    networkFailType: string;
+    okType: string;
+    sendType: string;
+    url: string;
+}
+
+
+function createFileFetchAction(config: CreateFetchConfig) {
+    const {
+        failType,
+        networkFailType,
+        okType,
+        sendType,
+        url,
+    } = config;
+
+    async function fetchAction() {
+        action(sendType);
+
+        const init: any = {
+            credentials: 'include',
+            method: 'get',
+        };
+
+        try {
+            const r: any = await fetch(url, init);
+
+            if (!r.ok) {
+                action(failType);
+            }
+
+            const blob = await r.blob();
+
+            const fileUrl = await URL.createObjectURL(blob);
+            window.open(fileUrl, '_blank');
+
+            const data = { file: blob };
+            action(okType, data);
+        } catch (e) {
+            if (e.message === 'Failed to fetch') {
+                action(networkFailType);
+            }
+
+            action('INTERNAL_ERROR');
+
+            throw e;
+        }
+    }
+
+    return fetchAction;
+}
+
+
+export default createFileFetchAction;
