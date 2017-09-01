8d33a5df99fbc199af9d976e6396c8c289944d87
Joe Ranweiler <joe@lemma.co>
2017-08-31 19:30:50 -0700

    Add county finish audit action.

---
+++ client/src/action/county/finishAudit.ts
@@ -0,0 +1,17 @@
+import { endpoint } from '../../config';
+
+import createSubmitAction from '../createSubmitAction';
+
+
+const url = endpoint('audit-report');
+
+const finishAudit = createSubmitAction({
+    failType: 'FINISH_AUDIT_FAIL',
+    networkFailType: 'FINISH_AUDIT_NETWORK_FAIL',
+    okType: 'FINISH_AUDIT_OK',
+    sendType: 'FINISH_AUDIT_SEND',
+    url,
+});
+
+
+export default () => finishAudit({});
