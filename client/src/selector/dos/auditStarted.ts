1d726b363dcabfd63df3074c45abc0a949b2f31c
Joe Ranweiler <joe@lemma.co>
2017-09-02 15:24:09 -0700

    Update DOS audit started selector.

--- client/src/selector/dos/auditStarted.ts
+++ client/src/selector/dos/auditStarted.ts
@@ -2,13 +2,19 @@ import * as _ from 'lodash';
 
 
 const STARTED_STATES = [
+    'BALLOT_ORDER_DEFINED',
     'AUDIT_READY_TO_START',
     'DOS_AUDIT_ONGOING',
+    'DOS_ROUND_COMPLETE',
     'DOS_AUDIT_COMPLETE',
     'AUDIT_RESULTS_PUBLISHED',
 ];
 
 function auditStarted(state: any): boolean {
+    if (!_.has(state, 'sos.asm.currentState')) {
+        return false;
+    }
+
     const { currentState } = state.sos.asm;
 
     return _.includes(STARTED_STATES, currentState);
