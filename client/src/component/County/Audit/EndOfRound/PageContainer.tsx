f57188736e51638ec9c971eef8fcd375b769f636
Joe Ranweiler <joe@lemma.co>
2017-09-05 20:18:55 -0700

    Remove dead page.

--- client/src/component/County/Audit/EndOfRound/PageContainer.tsx
+++ client/src/component/County/Audit/EndOfRound/PageContainer.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
 
-import EndOfLastRoundPage from './LastRoundPage';
 import EndOfRoundPage from './Page';
 
 import allRoundsCompleteSelector from 'corla/selector/county/allRoundsComplete';
