ceb8f4ea7f6e201c87ba9f5849218cd411dec483
Joe Ranweiler <joe@lemma.co>
2017-09-08 13:48:38 -0700

    Make logout action attempt to de-authenticate.

--- client/src/action/logout.ts
+++ client/src/action/logout.ts
@@ -1,4 +1,19 @@
+import { endpoint } from 'corla/config';
+
 import action from '.';
 
+import createSubmitAction from './createSubmitAction';
 
-export default () => action('LOGOUT');
+
+const url = endpoint('/unauthenticate');
+
+const submit = createSubmitAction({
+    failType: 'LOGOUT_FAIL',
+    networkFailType: 'LOGOUT_NETWORK_FAIL',
+    okType: 'LOGOUT_OK',
+    sendType: 'LOGOUT_SEND',
+    url,
+});
+
+
+export default () => submit({});
