77ab796756ede0950c98ca8d4dddcee0e4603a94
Joe Ranweiler <joe@lemma.co>
2017-09-11 15:03:33 -0700

    Display ratio of active counties who have finished round.
    Closes #650.

--- client/src/component/DOS/Dashboard/Round/Status.tsx
+++ client/src/component/DOS/Dashboard/Round/Status.tsx
@@ -4,11 +4,20 @@ import * as React from 'react';
 const Status = (props: any) => {
     const { activeCounties, currentRound } = props;
 
+    const totalCountiesCount = activeCounties.length;
+
+    const finished = (c: any) => c.auditBoardAsmState === 'WAITING_FOR_ROUND_START';
+    const finishedCountiesCount = activeCounties.filter(finished).length;
+
     return (
         <div className='pt-card'>
             <h4>Round status</h4>
             <div>
                 Round { currentRound } in progress.
+            </div>
+            <div>
+                { finishedCountiesCount } of { totalCountiesCount } Counties
+                have finished this round.
             </div>
         </div>
     );
