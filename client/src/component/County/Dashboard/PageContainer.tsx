ed7d2664fa177ce0ed9400318b72306f8652e6f8
Joe Ranweiler <joe@lemma.co>
2017-09-11 16:26:16 -0700

    Pass derived audit started state to county dashboard.

--- client/src/component/County/Dashboard/PageContainer.tsx
+++ client/src/component/County/Dashboard/PageContainer.tsx
@@ -10,6 +10,7 @@ import finishAudit from 'corla/action/county/finishAudit';
 import allRoundsComplete from 'corla/selector/county/allRoundsComplete';
 import auditBoardSignedIn from 'corla/selector/county/auditBoardSignedIn';
 import auditComplete from 'corla/selector/county/auditComplete';
+import auditStarted from 'corla/selector/county/auditStarted';
 import canAudit from 'corla/selector/county/canAudit';
 import canRenderReport from 'corla/selector/county/canRenderReport';
 import canSignIn from 'corla/selector/county/canSignIn';
@@ -19,6 +20,7 @@ class CountyDashboardContainer extends React.Component<any, any> {
     public render() {
         const {
             allRoundsComplete,
+            auditStarted,
             canAudit,
             canRenderReport,
             canSignIn,
@@ -32,6 +34,7 @@ class CountyDashboardContainer extends React.Component<any, any> {
 
         const props = {
             allRoundsComplete,
+            auditStarted,
             boardSignIn,
             canAudit,
             canRenderReport,
@@ -54,6 +57,7 @@ const mapStateToProps = (state: any) => {
         allRoundsComplete: allRoundsComplete(state),
         auditBoardSignedIn: auditBoardSignedIn(state),
         auditComplete: auditComplete(state),
+        auditStarted: auditStarted(state),
         canAudit: canAudit(state),
         canRenderReport: canRenderReport(state),
         canSignIn: canSignIn(state),
