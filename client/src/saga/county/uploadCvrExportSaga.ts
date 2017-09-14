480cca10e67581030cf44bfd3d4eaff2cfaacad9
Joe Ranweiler <joe@lemma.co>
2017-09-13 21:51:12 -0700

    Dispatch uploading CVR export actions in saga.

--- client/src/saga/county/uploadCvrExportSaga.ts
+++ client/src/saga/county/uploadCvrExportSaga.ts
@@ -1,4 +1,8 @@
-import { takeLatest } from 'redux-saga/effects';
+import {
+    put,
+    select,
+    takeLatest,
+} from 'redux-saga/effects';
 
 import notice from 'corla/notice';
 
@@ -47,6 +51,28 @@ function* uploadCvrExportNetworkFail(): IterableIterator<any> {
     notice.danger('Network error: failed to upload CVR export.');
 }
 
+function createUploadingCvrExport(uploading: boolean) {
+    function* uploadingCvrExport(action: any): IterableIterator<any> {
+        const data = { uploading };
+
+        yield put({ data, type: 'UPLOADING_CVR_EXPORT' });
+    }
+
+    return uploadingCvrExport;
+}
+
+const UPLOADING_FALSE = [
+    'IMPORT_CVR_EXPORT_FAIL',
+    'IMPORT_CVR_EXPORT_NETWORK_FAIL',
+    'IMPORT_CVR_EXPORT_OK',
+    'UPLOAD_CVR_EXPORT_FAIL',
+    'UPLOAD_CVR_EXPORT_NETWORK_FAIL',
+];
+
+const UPLOADING_TRUE = [
+    'UPLOAD_CVR_EXPORT_SEND',
+];
+
 
 export default function* uploadCvrExportSaga() {
     yield takeLatest('IMPORT_CVR_EXPORT_OK', importCvrExportOk);
@@ -57,4 +83,6 @@ export default function* uploadCvrExportSaga() {
     yield takeLatest('UPLOAD_CVR_EXPORT_FAIL', uploadCvrExportFail);
     yield takeLatest('UPLOAD_CVR_EXPORT_NETWORK_FAIL', uploadCvrExportNetworkFail);
 
+    yield takeLatest(UPLOADING_FALSE, createUploadingCvrExport(false));
+    yield takeLatest(UPLOADING_TRUE, createUploadingCvrExport(true));
 }
