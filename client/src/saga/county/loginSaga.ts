fbb80f51b630d6f7e240330d5a925be55b51caff
Joe Ranweiler <joe@lemma.co>
2017-09-19 14:00:03 -0700

    Don't fetch county data on login.

--- client/src/saga/county/loginSaga.ts
+++ client/src/saga/county/loginSaga.ts
@@ -4,20 +4,8 @@ import {
     takeLatest,
 } from 'redux-saga/effects';
 
-import countyDashboardRefresh from 'corla/action/county/dashboardRefresh';
-import fetchAuditBoardAsmState from 'corla/action/county/fetchAuditBoardAsmState';
-import fetchCountyAsmState from 'corla/action/county/fetchCountyAsmState';
-
 import notice from 'corla/notice';
 
-
-function* countyLoginOk() {
-    countyDashboardRefresh();
-    fetchAuditBoardAsmState();
-    fetchCountyAsmState();
-
-    yield put({ type: 'COUNTY_POLL' });
-}
 
 function* countyLoginFail(): IterableIterator<void> {
     notice.danger('Invalid credentials.');
@@ -31,5 +19,4 @@ function* countyLoginNetworkFail(): IterableIterator<void> {
 export default function* countyLoginSaga() {
     yield takeLatest('COUNTY_LOGIN_FAIL', countyLoginFail);
     yield takeLatest('COUNTY_LOGIN_NETWORK_FAIL', countyLoginNetworkFail);
-    yield takeLatest('COUNTY_LOGIN_OK', countyLoginOk);
 }
