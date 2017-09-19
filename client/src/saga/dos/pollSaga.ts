1eeaabae9bacd94a2efbce0179478847b9da67de
Joe Ranweiler <joe@lemma.co>
2017-09-19 10:16:10 -0700

    Reorganize DOS poll saga.

--- client/src/saga/dos/pollSaga.ts
+++ client/src/saga/dos/pollSaga.ts
@@ -1,5 +1,5 @@
 import { delay } from 'redux-saga';
-import { cancel, fork, take, takeLatest } from 'redux-saga/effects';
+import { all, cancel, fork, take, takeLatest } from 'redux-saga/effects';
 
 import dashboardRefresh from 'corla/action/dos/dashboardRefresh';
 import fetchContests from 'corla/action/dos/fetchContests';
@@ -7,7 +7,7 @@ import fetchContests from 'corla/action/dos/fetchContests';
 
 const DOS_POLL_DELAY = 1000 * 5;
 
-function* pollTask() {
+function* dosDashboardPoll() {
     while (true) {
         dashboardRefresh();
         fetchContests();
@@ -16,17 +16,23 @@ function* pollTask() {
     }
 }
 
-
-export default function* pollSaga() {
+function* dosDashboardPollSaga() {
     let poll = yield take('DOS_DASHBOARD_POLL_START')
 
     while (poll) {
-        const t = yield fork(pollTask);
+        const task = yield fork(dosDashboardPoll);
 
         yield take('DOS_DASHBOARD_POLL_STOP');
 
-        yield cancel(t);
+        yield cancel(task);
 
         poll = yield take('DOS_DASHBOARD_POLL_START')
     }
+}
+
+
+export default function* pollSaga() {
+    yield all([
+        dosDashboardPollSaga(),
+    ]);
 }
