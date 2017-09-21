971b5a38d20793e176b3f4156b090aec95f80357
Joe Ranweiler <joe@lemma.co>
2017-09-21 13:13:34 -0700

    Add DOS set hand count action.

---
+++ client/src/action/dos/setHandCount.ts
@@ -0,0 +1,24 @@
+import { endpoint } from 'corla/config';
+
+import createSubmitAction from 'corla/action/createSubmitAction';
+
+
+const url = endpoint('hand-count');
+
+const setHandCount = createSubmitAction({
+    failType: 'SET_HAND_COUNT_FAIL',
+    networkFailType: 'SET_HAND_COUNT_NETWORK_FAIL',
+    okType: 'SET_HAND_COUNT_OK',
+    sendType: 'SET_HAND_COUNT_SEND',
+    url,
+});
+
+function format(id: number) {
+    return [{
+        audit: 'HAND_COUNT',
+        contest: id,
+        reason: 'OPPORTUNISTIC_BENEFITS',
+    }];
+}
+
+export default (data: any) => setHandCount(format(data));
