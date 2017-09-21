7a0abcead88e048bfd588f216f31ef5b78a669cf
Joe Ranweiler <joe@lemma.co>
2017-09-21 11:26:26 -0700

    Render audit board in DOS county detail page.
    Closes #716.

--- client/src/component/DOS/County/DetailPage.tsx
+++ client/src/component/DOS/County/DetailPage.tsx
@@ -27,39 +27,94 @@ const Breadcrumb = ({ county }: any) => (
     </ul>
 );
 
-const CountyDetails = ({ county, status }: any) => {
+function formatMember(member: any): string {
+    const { firstName, lastName, party } = member;
+
+    return `${firstName} ${lastName} (${party})`;
+}
+
+const AuditBoard = (props: any) => {
+    const { auditBoard } = props;
+
+    return (
+        <div className='pt-card'>
+            <h3>Audit Board</h3>
+            <table className='pt-table pt-bordered pt-condensed'>
+                <tbody>
+                    <tr>
+                        <td><strong>Board Member #1:</strong></td>
+                        <td>{ formatMember(auditBoard.members[0]) }</td>
+                    </tr>
+                    <tr>
+                        <td><strong>Board Member #2:</strong></td>
+                        <td>{ formatMember(auditBoard.members[1]) }</td>
+                    </tr>
+                    <tr>
+                        <td><strong>Sign-in Time:</strong></td>
+                        <td>{ `${auditBoard.signIn}` }</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+const NoAuditBoard = (props: any) => {
+    return (
+        <div className='pt-card'>
+            <h3>Audit Board</h3>
+            <div className='pt-card'>
+                Audit Board not signed in.
+            </div>
+        </div>
+    );
+};
+
+const CountyDetails = (props: any) => {
+    const { county, status } = props;
+    const { auditBoard } = status;
+
     const countyState = formatCountyAsmState(status.asmState);
     const submitted = status.auditedBallotCount;
 
     const auditedCount = _.get(status, 'discrepancyCount.audited') || '—';
     const oppCount = _.get(status, 'discrepancyCount.opportunistic') || '—';
 
+
+    const auditBoardSection = auditBoard
+                            ? <AuditBoard auditBoard={ auditBoard } />
+                            : <NoAuditBoard />;
+
     return (
         <div className='pt-card'>
-            <table className='pt-table pt-bordered pt-condensed'>
-                <tbody>
-                    <tr>
-                        <td><strong>Name:</strong></td>
-                        <td>{ county.name }</td>
-                    </tr>
-                    <tr>
-                        <td><strong>Status:</strong></td>
-                        <td>{ countyState }</td>
-                    </tr>
-                    <tr>
-                        <td><strong>Ballots Submitted:</strong></td>
-                        <td>{ submitted }</td>
-                    </tr>
-                    <tr>
-                        <td><strong>Audited Contest Discrepancies:</strong></td>
-                        <td>{ auditedCount }</td>
-                    </tr>
-                    <tr>
-                        <td><strong>Non-audited Contest Discrepancies:</strong></td>
-                        <td>{ oppCount }</td>
-                    </tr>
-                </tbody>
-            </table>
+            <div className='pt-card'>
+                <h3>County Info</h3>
+                <table className='pt-table pt-bordered pt-condensed'>
+                    <tbody>
+                        <tr>
+                            <td><strong>Name:</strong></td>
+                            <td>{ county.name }</td>
+                        </tr>
+                        <tr>
+                            <td><strong>Status:</strong></td>
+                            <td>{ countyState }</td>
+                        </tr>
+                        <tr>
+                            <td><strong>Ballots Submitted:</strong></td>
+                            <td>{ submitted }</td>
+                        </tr>
+                        <tr>
+                            <td><strong>Audited Contest Discrepancies:</strong></td>
+                            <td>{ auditedCount }</td>
+                        </tr>
+                        <tr>
+                            <td><strong>Non-audited Contest Discrepancies:</strong></td>
+                            <td>{ oppCount }</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            { auditBoardSection }
         </div>
     );
 };
