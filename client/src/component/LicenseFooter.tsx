1f022459c652bce10da6ebcee00c6d58d79d2263
Joe Ranweiler <joe@lemma.co>
2017-09-29 16:50:10 -0700

    Add license footer component and CSS.

---
+++ client/src/component/LicenseFooter.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+
+
+const projectUrl = 'https://github.com/FreeAndFair/ColoradoRLA';
+
+const License = () => {
+    return (
+        <div className='pt-card rla-license'>
+            The <em>Colorado RLA Tool</em> is Copyright (C) 2017 the Colorado Department of
+            State and Free & Fair, and is licensed under the AGPLv3 with a classpath exception.
+            See the project's <a href={ projectUrl }>GitHub site</a> for more information.
+        </div>
+    );
+};
+
+const LicenseFooter = () => {
+    return (
+        <div className='rla-license-footer' >
+            <License />
+        </div>
+    );
+};
+
+
+export default LicenseFooter;
