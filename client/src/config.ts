1a4a56a7477840b69ad4d183bd61265e7c2c74e9
Joe Ranweiler <joe@lemma.co>
2017-10-02 19:56:37 -0700

    Set default election date to current date.

--- client/src/config.ts
+++ client/src/config.ts
@@ -13,8 +13,6 @@ export const endpoint = (path: string) => `${endpointPrefix}/${path}`;
 
 export const timezone = 'America/Denver';
 
-export const defaultElectionDate = '2017-11-07';
-
 export const defaultPublicMeetingDate = '2017-11-17';
 
 export const helpEmail = 'help@example.com';
