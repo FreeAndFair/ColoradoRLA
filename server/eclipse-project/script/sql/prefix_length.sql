89b1c90b944ed395ad69f905e5d5a1593aa2ae22
Neal McBurnett <nealmcb@freeandfair.us>
2017-10-04 22:42:35 -0600

    fix prefix_length.sql, clean up __main__.py

--- server/eclipse-project/script/sql/prefix_length.sql
+++ server/eclipse-project/script/sql/prefix_length.sql
@@ -1,9 +1,8 @@
 -- List, by county name, of prefix length (to end of current round)
--- FIXME - is this true?  In a county that doesn't have an audit, ignore the prefix_length of 1.
 
-SELECT co.name as county_name, count(*) as prefix_length 
+SELECT co.name as county_name, cd.audited_prefix_length 
 FROM county AS co
-LEFT JOIN cvr_audit_info AS cai
-ON co.id = cai.dashboard_id
-GROUP BY co.name ORDER BY co.name
+LEFT JOIN county_dashboard AS cd
+ON co.id = cd.county_id
+ORDER BY co.name
 ;
