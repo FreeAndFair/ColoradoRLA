ba24cedd7d56e463cb53b28b28896cba18e71b64
Joe Ranweiler <joe@lemma.co>
2017-10-05 22:33:05 -0700

    Add sorting to DOS contest updates.

--- client/src/component/DOS/Dashboard/ContestUpdates.tsx
+++ client/src/component/DOS/Dashboard/ContestUpdates.tsx
@@ -37,50 +37,113 @@ const HandCountButton = (props: any) => {
     );
 };
 
-const ContestUpdates = ({ contests, seed, sos }: any) => {
-    const selectedContests = _.pick(contests, _.keys(sos.auditedContests));
+type SortKey = 'county'
+             | 'name'
+             | 'discrepancyCount';
 
-    const contestStatuses = _.map(selectedContests, (c: any) => {
-        if (!sos.auditedContests) {
-            return <tr key={ c.id }><td /><td /><td /><td /><td /></tr>;
+function sortIndex(sort: SortKey): number {
+    // tslint:disable
+    const index: any = {
+        county: 0,
+        name: 1,
+        discrepancyCount: 2,
+    };
+    // tslint:enable
+
+    return index[sort];
+}
+
+class ContestUpdates extends React.Component<any, any> {
+    public state: any = {
+        order: 'asc',
+        sort: 'name',
+    };
+
+    public render() {
+        const { contests, seed, sos } = this.props;
+
+        const selectedContests = _.values(_.pick(contests, _.keys(sos.auditedContests)));
+
+        const rowData = _.map(selectedContests, (c: any) => {
+            const county = counties[c.countyId];
+            const discrepancyCount = _.sum(_.values(sos.discrepancyCounts[c.id]));
+
+            return [county.name, c.name, discrepancyCount, c];
+        });
+
+        const keyFunc = (d: any[]) => d[sortIndex(this.state.sort)];
+        const sortedData = _.sortBy(rowData, keyFunc);
+
+        if (this.state.order === 'desc') {
+            _.reverse(sortedData);
         }
 
-        const county = counties[c.countyId];
-        const discrepancyCount = _.sum(_.values(sos.discrepancyCounts[c.id]));
+        const contestStatuses = _.map(sortedData, (row: any) => {
+            const [countyName, contestName, discrepancyCount, c] = row;
+
+            if (!sos.auditedContests) {
+                return <tr key={ c.id }><td /><td /><td /><td /><td /></tr>;
+            }
+
+            return (
+                <tr key={ c.id }>
+                    <td>
+                        <HandCountButton contest={ c } />
+                    </td>
+                    <td>{ countyName }</td>
+                    <td>{ contestName }</td>
+                    <td>{ discrepancyCount }</td>
+                </tr>
+            );
+        });
 
         return (
-            <tr key={ c.id }>
-                <td>
-                    <HandCountButton contest={ c } />
-                </td>
-                <td>{ county.name }</td>
-                <td>{ c.name }</td>
-                <td>{ discrepancyCount }</td>
-            </tr>
-        );
-    });
-
-    return (
-        <div className='pt-card'>
-            <h3>Contest Updates</h3>
             <div className='pt-card'>
-                <table className='pt-table'>
-                    <thead>
-                        <tr>
-                            <th>Hand Count</th>
-                            <th>County</th>
-                            <th>Name</th>
-                            <th>Discrepancies</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        { ...contestStatuses }
-                    </tbody>
-                </table>
+                <h3>Contest Updates</h3>
+                <div className='pt-card'>
+                    <table className='pt-table'>
+                        <thead>
+                            <tr>
+                                <th>Hand Count</th>
+                                <th onClick={ this.sortBy('county') }>
+                                    County
+                                </th>
+                                <th onClick={ this.sortBy('name') }>
+                                    Name
+                                </th>
+                                <th onClick={ this.sortBy('discrepancyCount') }>
+                                    Discrepancies
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            { ...contestStatuses }
+                        </tbody>
+                    </table>
+                </div>
             </div>
-        </div>
-    );
-};
+        );
+    }
+
+    private sortBy(sort: SortKey) {
+        return () => {
+            if (this.state.sort === sort) {
+                this.reverseOrder();
+            } else {
+                const order = 'asc';
+                this.setState({ sort, order });
+            }
+        };
+    }
+
+    private reverseOrder() {
+        const order = this.state.order === 'asc'
+                    ? 'desc'
+                    : 'asc';
+
+        this.setState({ order });
+    }
+}
 
 
 export default ContestUpdates;
