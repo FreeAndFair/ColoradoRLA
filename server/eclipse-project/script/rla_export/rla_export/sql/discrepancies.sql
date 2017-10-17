f8280aa015619e5b1d5e9f7e807910fa443f2e97
Neal McBurnett <nealmcb@freeandfair.us>
2017-10-17 16:05:16 -0600

    discrepancy.sql: incorporate more fields, sorting
    Allows for use by main.py RLA confirmation also

--- server/eclipse-project/script/rla_export/rla_export/sql/discrepancies.sql
+++ server/eclipse-project/script/rla_export/rla_export/sql/discrepancies.sql
@@ -1,28 +1,37 @@
 -- Retrieve counts of audited ballot cards and each type of discrepancy by contest
+-- along with contest ballot counts, outcomes and margins for checking calculations.
 
-SELECT 
-  contest.name, 
-  contest.id, 
-  contest.winners_allowed, 
-  county_contest_comparison_audit.one_vote_over_count, 
-  county_contest_comparison_audit.one_vote_under_count, 
-  county_contest_comparison_audit.two_vote_over_count, 
-  county_contest_comparison_audit.two_vote_under_count, 
-  county_contest_comparison_audit.id, 
-  county_contest_comparison_audit.audit_reason, 
-  county_contest_comparison_audit.audit_status, 
-  county_contest_comparison_audit.audited_sample_count, 
-  county_contest_comparison_audit.disagreement_count, 
-  county_contest_comparison_audit.estimated_samples_to_audit, 
-  county_contest_comparison_audit.estimated_recalculate_needed, 
-  county_contest_comparison_audit.gamma, 
-  county_contest_comparison_audit.optimistic_recalculate_needed, 
-  county_contest_comparison_audit.optimistic_samples_to_audit, 
-  county_contest_comparison_audit.risk_limit, 
-  county_contest_comparison_audit.dashboard_id
-FROM 
-  public.county_contest_comparison_audit, 
-  public.contest
-WHERE 
-  county_contest_comparison_audit.contest_id = contest.id
+SELECT
+  contest.name,
+  contest.id,
+  contest.winners_allowed,
+  county_contest_comparison_audit.one_vote_over_count,
+  county_contest_comparison_audit.one_vote_under_count,
+  county_contest_comparison_audit.two_vote_over_count,
+  county_contest_comparison_audit.two_vote_under_count,
+  county_contest_comparison_audit.id,
+  county_contest_comparison_audit.audit_reason,
+  county_contest_comparison_audit.audit_status,
+  county_contest_comparison_audit.audited_sample_count,
+  county_contest_comparison_audit.disagreement_count,
+  county_contest_comparison_audit.estimated_samples_to_audit,
+  county_contest_comparison_audit.estimated_recalculate_needed,
+  county_contest_comparison_audit.gamma,
+  county_contest_comparison_audit.optimistic_recalculate_needed,
+  county_contest_comparison_audit.optimistic_samples_to_audit,
+  county_contest_comparison_audit.risk_limit,
+  contest.county_id,
+  county_contest_result.min_margin,
+  county_contest_result.winners,
+  county_contest_result.losers,
+  county_contest_result.county_ballot_count,
+  county_contest_result.contest_ballot_count
+FROM
+  county_contest_comparison_audit,
+  contest,
+  county_contest_result
+WHERE
+  county_contest_comparison_audit.contest_id = contest.id AND
+  county_contest_comparison_audit.contest_result_id = county_contest_result.id
+ORDER BY contest.county_id
 ;
