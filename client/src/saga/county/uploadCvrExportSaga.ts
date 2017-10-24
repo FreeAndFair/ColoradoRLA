15cedb6ef1999f10dae70fea1b3b302f570797e6
Joe Ranweiler <joe@lemma.co>
2017-10-24 16:06:18 -0700

    Alert user of ok CVR import based on ASM, not XHR.
    CVR uploads are synchronous, but parsing/_import_ is now asynchronous,
    so the import action returns immediately, with status and progress
    made available in the county refresh call.

--- client/src/saga/county/uploadCvrExportSaga.ts
+++ client/src/saga/county/uploadCvrExportSaga.ts
@@ -7,13 +7,6 @@ import {
 import notice from 'corla/notice';
 
 
-function* importCvrExportOk(action: any): IterableIterator<any> {
-    const { data } = action;
-    const { sent } = data;
-
-    notice.ok(`Imported CVR export "${sent.filename}".`);
-}
-
 function* importCvrExportFail(action: any): IterableIterator<any> {
     const { data } = action;
     const { received, sent } = data;
@@ -71,7 +64,6 @@ const UPLOADING_TRUE = [
 
 
 export default function* uploadCvrExportSaga() {
-    yield takeLatest('IMPORT_CVR_EXPORT_OK', importCvrExportOk);
     yield takeLatest('IMPORT_CVR_EXPORT_FAIL', importCvrExportFail);
     yield takeLatest('IMPORT_CVR_EXPORT_NETWORK_FAIL', importCvrExportNetworkFail);
 
