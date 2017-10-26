390d1653e26976ef7d57ef0f0333ca7dc8919c71
Joe Ranweiler <joe@lemma.co>
2017-10-26 11:47:26 -0700

    Update react-redux type decls, handle possible null.

--- client/src/component/County/Audit/Wizard/ReviewStageContainer.tsx
+++ client/src/component/County/Audit/Wizard/ReviewStageContainer.tsx
@@ -8,26 +8,47 @@ import uploadAcvr from 'corla/action/county/uploadAcvr';
 
 interface ContainerProps {
     countyState: County.AppState;
-    currentBallot: CVR;
-    marks: County.ACVR;
+    currentBallot?: CVR;
+    marks?: County.ACVR;
     nextStage: OnClick;
     prevStage: OnClick;
 }
 
 class ReviewStageContainer extends React.Component<ContainerProps> {
     public render() {
-        const props = { ...this.props, uploadAcvr };
+        const {
+            countyState,
+            currentBallot,
+            marks,
+            nextStage,
+            prevStage,
+        } = this.props;
 
-        return <ReviewStage { ...props } />;
+        if (!currentBallot) {
+            return null;
+        }
+
+        if (!marks) {
+            return null;
+        }
+
+        return <ReviewStage countyState={ countyState }
+                            currentBallot={ currentBallot }
+                            marks={ marks }
+                            nextStage={ nextStage }
+                            prevStage={ prevStage }
+                            uploadAcvr={ uploadAcvr } />;
     }
 }
 
 function select(countyState: County.AppState) {
     const { currentBallot } = countyState;
 
-    if (!currentBallot) { return {}; }
+    if (!currentBallot) {
+        return { countyState };
+    }
 
-    const marks = countyState!.acvrs![currentBallot.id];
+    const marks = countyState.acvrs[currentBallot.id];
 
     return { countyState, currentBallot, marks };
 }
