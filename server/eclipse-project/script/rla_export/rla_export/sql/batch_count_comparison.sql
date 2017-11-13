7dc9f6dcbe17aec06f89e8a72a2e5f5290bb202f
Neal McBurnett <nealmcb@freeandfair.us>
2017-11-13 14:03:08 -0700

    Rename, reformat batch_count_comparison

--- server/eclipse-project/script/rla_export/rla_export/sql/batch_count_comparison.sql
+++ server/eclipse-project/script/rla_export/rla_export/sql/batch_count_comparison.sql
@@ -1,7 +1,15 @@
 -- For each county and each batch, compare batch counts reflected in manifest vs CVR file.
 
-SELECT bmi.county_id,bmi.scanner_id,bmi.batch_id, bmi.batch_size, count(cvr.id) as count_cvr, (bmi.batch_size -count(cvr.id)) as discrepancy from ballot_manifest_info AS bmi
-    LEFT JOIN cast_vote_record AS cvr
+SELECT
+   bmi.county_id,
+   bmi.scanner_id,
+   bmi.batch_id,
+   bmi.batch_size as count_per_manifest,
+   count(cvr.id) as count_per_cvr_file,
+   (bmi.batch_size -count(cvr.id)) as difference
+
+FROM ballot_manifest_info AS bmi
+ LEFT JOIN cast_vote_record AS cvr
         ON bmi.county_id = cvr.county_id 
             AND bmi.batch_id = cvr.batch_id
             AND bmi.scanner_id = cvr.scanner_id
