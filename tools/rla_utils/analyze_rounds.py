9b9743f2aa239d1445ee736d73899e6c250ff100
Neal McBurnett <nealmcb@freeandfair.us>
2018-06-12 17:52:17 -0600

    Improve analyze_rounds formatting, all in stdout.
    And provide number of ACVRs entered.

--- tools/rla_utils/analyze_rounds.py
+++ tools/rla_utils/analyze_rounds.py
@@ -45,49 +45,61 @@ parser.add_argument("-d, --debuglevel", type=int, default=logging.WARNING, dest=
   help="Set logging level to debuglevel, expressed as an integer: "
   "DEBUG=10, INFO=20, endpoint timing=25, WARNING=30 (the default), ERROR=40, CRITICAL=50")
 
-parser.add_argument('exports', nargs='?',
-                    help='Directory with rla_export reports')
+parser.add_argument('datadir', nargs='?',
+                    help='Data directory with rla_export reports')
 
 
 def analyze_rounds(parser):
 
     args = parser.parse_args()
-    print("analyze_rounds: %s" % (args,))
+    logging.debug("analyze_rounds command-line arguments: %s" % (args,))
     # Read in contests, with winners and losers.
     #  Calculate from parse_hart.py?  Saved as json??
     #  Read in from rla_export all_contest_static.json?  No, that's based on wrong info. Could it be made right?
 
-    contests = json.load(open("/tmp/contests.json", "r"))
+    contests = json.load(open("contests.json", "r"))
 
     try:
-        acvrs = json.load(open(args.exports + "/all_contest_audit_details_by_cvr.json"))
-    except:
+        acvr_file = args.datadir + "/all_contest_audit_details_by_cvr.json"
+        acvrs = json.load(open(acvr_file))
+    except (IOError):
+        logging.warning("ACVR file %s error" % acvr_file, exc_info=True)
         acvrs = []
 
     # Tally acvrs.
     # FIXME: check for consensus = "YES"
     # FIXME: check for, ignore overvotes
     acvr_tallies = collections.Counter((cvr['contest_name'], cvr['choice_per_audit_board']) for cvr in acvrs).items()
-    print("acvr tallies: %s" % (acvr_tallies,))
+    logging.debug("acvr tallies: %s" % (acvr_tallies,))
+
+    print("ACVRs entered: %s" % collections.Counter(cvr['contest_name'] for cvr in acvrs).items())
+
+    print("\nHere are the non-zero tallies of the sampled ballots, by contest by choice.")
+    for key, result in sorted(collections.Counter((cvr['contest_name'], cvr['choice_per_audit_board']) for cvr in acvrs).items()):
+        print("%s\t%s\t%s" % (result, key[0], key[1]))
+
     for id, sample_votes in acvr_tallies:
         contest, choices = id
         for choice in json.loads(choices):
             contests[contest]['choices'][choice]['sample_tally'] = sample_votes
 
     # print(contests)
-    print("maxsample\t2nd_vs_3rd\toutright\tmaxrisk\toutrightrisk\tcandidates\tcontest")
+    print("\nDetails then Status by contest: sample sizes for two outcomes and max of them; risk levels for each")
     for contest in contests.values():
         # print margin
         logging.debug("Contest with %d candidates: %s" % (len(contest['choices']), contest['name']))
         if 'selected' not in contest:
             continue
+
+        print("\nContest: %s, with %d candidates" % (contest['name'], len(contest['choices'])))
+
         contest['risk_levels'] = []
         for winner in contest['winners']:
             w = contests[contest['name']]['choices'][winner]
             for loser in contest['losers']:
                 l = contests[contest['name']]['choices'][loser]
                 risk_level = rlacalc.ballot_polling_risk_level(w['votes'], l['votes'], w.get('sample_tally', 0), l.get('sample_tally', 0))
-                logging.warning("%.3f for %s: %d %d %d %d in %s vs %s" % (risk_level, contest['name'], w['votes'], l['votes'], w.get('sample_tally', 0), l.get('sample_tally', 0), w['name'], l['name']))
+                print("\tdetail:  Risk %.3f with counts W: %d L: %d w: %d l: %d for %s vs %s" % (risk_level, w['votes'], l['votes'], w.get('sample_tally', 0), l.get('sample_tally', 0), w['name'], l['name']))
                 contest['risk_levels'].append((risk_level, w, l))
                 # contests[contest['name']]['choices'][choice]['risk_level'] = risk_level
                 # Track minimum risk level across all combinations
@@ -113,23 +125,24 @@ def analyze_rounds(parser):
 
         majority_risk = rlacalc.ballot_polling_risk_level(w_votes, pool_votes, w_sample_tallies, pool_sample_tallies)
         contest_name = contest['name']
-        logging.warning("risk {majority_risk} for {contest_name}: {w_votes}, {w_sample_tallies}  vs {pool_votes}, {pool_sample_tallies}".format(**locals()))
-        max_risk = max(max_risk, majority_risk)
+        print("\tMajority risk {majority_risk:.3f} with counts W: {w_votes} L: {pool_votes} w: {w_sample_tallies} l: {pool_sample_tallies}".format(**locals()))
+        overall_max_risk = max(max_risk, majority_risk) # TODO - unused for now
 
         # Compute an initial mean sample size.
         risk_limit = 0.2 # TODO: set as an option
         sample_size = rlacalc.findAsn(risk_limit, contest['margin'], max_risk)
-        sample_size_maj = rlacalc.findAsn(risk_limit, contest['majority_margin'], majority_risk)
+        sample_size_maj = rlacalc.findAsn(risk_limit, abs(contest['majority_margin']), majority_risk)
         max_sample_size = max(sample_size, sample_size_maj)
-        print("%.0f\t%.0f\t%.0f\t%.3f\t%.3f\t%s\t%s" % (max_sample_size, sample_size, sample_size_maj, max_risk, majority_risk, len(contest['choices']), contest['name']))
+        print("\n\tmaxsamp\t2_vs_3\twin_now\tmaxrisk\twinrisk\tcands\tcontest")
+        print("overall\t%.0f\t%.0f\t%.0f\t%.3f\t%.3f\t%s\t%s" % (max_sample_size, sample_size, sample_size_maj, max_risk, majority_risk, len(contest['choices']), contest['name']))
         # print("2nd vs 3rd: %d mean sample size, %.3f max risk for contest with %d candidates: %s" % (sample_size, max_risk, len(contest['choices']), contest['name']))
         # print("Majority: %d mean sample size, %.3f risk for contest with %d candidates: %s" % (sample_size_maj, majority_risk, len(contest['choices']), contest['name']))
     # Report minumum across all contests, then levels for all contests
 
-    # debugging
-    for key, result in sorted(collections.Counter((cvr['contest_name'], cvr['choice_per_audit_board']) for cvr in acvrs).items()):
-        print("%s\t%s\t%s" % (result, key[0], key[1]))
-
 
 if __name__ == "__main__":
+    args = parser.parse_args()
+    logging.basicConfig(level=args.debuglevel)
+    logging.info("analyze_rounds: %s" % (args,))
+
     analyze_rounds(parser)
