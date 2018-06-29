63f0d744894d9a3e2166b707a5d49c1547d06808
Neal McBurnett <nealmcb@freeandfair.us>
2018-06-29 08:37:20 -0600

    Add contests parameter, refactor contest_risk()

--- tools/rla_utils/analyze_rounds.py
+++ tools/rla_utils/analyze_rounds.py
@@ -26,6 +26,8 @@ TODO: check also for sampling with replacement
 from __future__ import print_function
 from __future__ import division
 
+import random
+
 import math
 import os
 import sys
@@ -45,26 +47,82 @@ parser.add_argument("-d, --debuglevel", type=int, default=logging.WARNING, dest=
   help="Set logging level to debuglevel, expressed as an integer: "
   "DEBUG=10, INFO=20, endpoint timing=25, WARNING=30 (the default), ERROR=40, CRITICAL=50")
 
+parser.add_argument('contests', nargs='?', default="contests.json",
+                    help='contests.json file, default "contests.json"')
 parser.add_argument('datadir', nargs='?',
                     help='Data directory with rla_export reports')
+
+
+def contest_risk(contest, contests):
+    # print margin
+    logging.debug("Contest with %d candidates: %s" % (len(contest['choices']), contest['name']))
+    if 'selected' not in contest:
+        return
+
+    print("\nContest: %s, with %d candidates" % (contest['name'], len(contest['choices'])))
+
+    contest['risk_levels'] = []
+    for winner in contest['winners']:
+        w = contests[contest['name']]['choices'][winner]
+        for loser in contest['losers']:
+            l = contests[contest['name']]['choices'][loser]
+            risk_level = rlacalc.ballot_polling_risk_level(w['votes'], l['votes'], w.get('sample_tally', 0), l.get('sample_tally', 0))
+            print("\tdetail:  Risk %.3f with counts W: %d L: %d w: %d l: %d for %s vs %s" % (risk_level, w['votes'], l['votes'], w.get('sample_tally', 0), l.get('sample_tally', 0), w['name'], l['name']))
+            contest['risk_levels'].append((risk_level, w, l))
+            # contests[contest['name']]['choices'][choice]['risk_level'] = risk_level
+            # Track minimum risk level across all combinations
+            # print("%s %s %s: %s" % (contest, w, l, ballot_polling_risk_level(winner_votes, loser_votes, winner_obs, loser_obs)))
+            # If minimum is more than risk level, estimate how much more to do
+    if contest['risk_levels']:
+        max_risk = max(triple[0] for triple in contest['risk_levels'])
+    else:
+        max_risk = 0.0
+
+    # Deal with single outright majority winner outcome: winner vs all others combined in a pool
+        w = contests[contest['name']]['choices'][contest['winners'][0]]
+    w_votes = w['votes']
+    w_sample_tallies = w.get('sample_tally', 0)
+    pool = [choice for choice in contests[contest['name']]['choices'].values() if choice['name'] != w['name']]
+    pool_votes = sum(choice['votes'] for choice in pool)
+    pool_sample_tallies = sum(choice.get('sample_tally', 0) for choice in pool)
+
+    # If highest-vote-getter got less than 50%, swap with the sum of the pool of losers
+    if w_votes < pool_votes:
+        w_votes, pool_votes = pool_votes , w_votes
+        w_sample_tallies, pool_sample_tallies = pool_sample_tallies, w_sample_tallies
+
+    majority_risk = rlacalc.ballot_polling_risk_level(w_votes, pool_votes, w_sample_tallies, pool_sample_tallies)
+    contest_name = contest['name']
+    print("\tMajority risk {majority_risk:.3f} with counts W: {w_votes} L: {pool_votes} w: {w_sample_tallies} l: {pool_sample_tallies}".format(**locals()))
+    overall_max_risk = max(max_risk, majority_risk) # TODO - unused for now
+
+    # Compute an initial mean sample size.
+    risk_limit = 0.2 # TODO: set as an option
+    sample_size = rlacalc.findAsn(risk_limit, contest['margin'], max_risk)
+    sample_size_maj = rlacalc.findAsn(risk_limit, abs(contest['majority_margin']), majority_risk)
+    max_sample_size = max(sample_size, sample_size_maj)
+    print("\n\tmaxsamp\t2_vs_3\twin_now\tmaxrisk\twinrisk\tcands\tcontest")
+    print("overall\t%.0f\t%.0f\t%.0f\t%.3f\t%.3f\t%s\t%s" % (max_sample_size, sample_size, sample_size_maj, max_risk, majority_risk, len(contest['choices']), contest['name']))
+    # print("2nd vs 3rd: %d mean sample size, %.3f max risk for contest with %d candidates: %s" % (sample_size, max_risk, len(contest['choices']), contest['name']))
+    # print("Majority: %d mean sample size, %.3f risk for contest with %d candidates: %s" % (sample_size_maj, majority_risk, len(contest['choices']), contest['name']))
+
 
 
 def analyze_rounds(parser):
 
     args = parser.parse_args()
     logging.debug("analyze_rounds command-line arguments: %s" % (args,))
+
     # Read in contests, with winners and losers.
-    #  Calculate from parse_hart.py?  Saved as json??
-    #  Read in from rla_export all_contest_static.json?  No, that's based on wrong info. Could it be made right?
+    contests = json.load(open(args.contests, "r"))
 
-    contests = json.load(open("contests.json", "r"))
-
-    try:
-        acvr_file = args.datadir + "/all_contest_audit_details_by_cvr.json"
-        acvrs = json.load(open(acvr_file))
-    except (IOError):
-        logging.warning("ACVR file %s error" % acvr_file, exc_info=True)
-        acvrs = []
+    acvrs = []
+    if args.datadir:
+        try:
+            acvr_file = args.datadir + "/all_contest_audit_details_by_cvr.json"
+            acvrs = json.load(open(acvr_file))
+        except (IOError):
+            logging.warning("Can't open ACVR file %s, assuming there are none" % acvr_file)
 
     # Tally acvrs.
     # FIXME: check for consensus = "YES"
@@ -78,65 +136,18 @@ def analyze_rounds(parser):
     for key, result in sorted(collections.Counter((cvr['contest_name'], cvr['choice_per_audit_board']) for cvr in acvrs).items()):
         print("%s\t%s\t%s" % (result, key[0], key[1]))
 
+    # Update contests data structure with sample_tally data
     for id, sample_votes in acvr_tallies:
         contest, choices = id
         for choice in json.loads(choices):
             contests[contest]['choices'][choice]['sample_tally'] = sample_votes
 
-    # print(contests)
+    logging.debug("Contests: %s" % contests)
+
     print("\nDetails then Status by contest: sample sizes for two outcomes and max of them; risk levels for each")
     for contest in contests.values():
-        # print margin
-        logging.debug("Contest with %d candidates: %s" % (len(contest['choices']), contest['name']))
-        if 'selected' not in contest:
-            continue
+        contest_risk(contest, contests)
 
-        print("\nContest: %s, with %d candidates" % (contest['name'], len(contest['choices'])))
-
-        contest['risk_levels'] = []
-        for winner in contest['winners']:
-            w = contests[contest['name']]['choices'][winner]
-            for loser in contest['losers']:
-                l = contests[contest['name']]['choices'][loser]
-                risk_level = rlacalc.ballot_polling_risk_level(w['votes'], l['votes'], w.get('sample_tally', 0), l.get('sample_tally', 0))
-                print("\tdetail:  Risk %.3f with counts W: %d L: %d w: %d l: %d for %s vs %s" % (risk_level, w['votes'], l['votes'], w.get('sample_tally', 0), l.get('sample_tally', 0), w['name'], l['name']))
-                contest['risk_levels'].append((risk_level, w, l))
-                # contests[contest['name']]['choices'][choice]['risk_level'] = risk_level
-                # Track minimum risk level across all combinations
-                # print("%s %s %s: %s" % (contest, w, l, ballot_polling_risk_level(winner_votes, loser_votes, winner_obs, loser_obs)))
-                # If minimum is more than risk level, estimate how much more to do
-        if contest['risk_levels']:
-            max_risk = max(triple[0] for triple in contest['risk_levels'])
-        else:
-            max_risk = 0.0
-
-        # Deal with single outright majority winner outcome: winner vs all others combined in a pool
-        w = contests[contest['name']]['choices'][contest['winners'][0]]
-        w_votes = w['votes']
-        w_sample_tallies = w.get('sample_tally', 0)
-        pool = [choice for choice in contests[contest['name']]['choices'].values() if choice['name'] != w['name']]
-        pool_votes = sum(choice['votes'] for choice in pool)
-        pool_sample_tallies = sum(choice.get('sample_tally', 0) for choice in pool)
-
-        # If highest-vote-getter got less than 50%, swap with the sum of the pool of losers
-        if w_votes < pool_votes:
-            w_votes, pool_votes = pool_votes , w_votes
-            w_sample_tallies, pool_sample_tallies = pool_sample_tallies, w_sample_tallies
-
-        majority_risk = rlacalc.ballot_polling_risk_level(w_votes, pool_votes, w_sample_tallies, pool_sample_tallies)
-        contest_name = contest['name']
-        print("\tMajority risk {majority_risk:.3f} with counts W: {w_votes} L: {pool_votes} w: {w_sample_tallies} l: {pool_sample_tallies}".format(**locals()))
-        overall_max_risk = max(max_risk, majority_risk) # TODO - unused for now
-
-        # Compute an initial mean sample size.
-        risk_limit = 0.2 # TODO: set as an option
-        sample_size = rlacalc.findAsn(risk_limit, contest['margin'], max_risk)
-        sample_size_maj = rlacalc.findAsn(risk_limit, abs(contest['majority_margin']), majority_risk)
-        max_sample_size = max(sample_size, sample_size_maj)
-        print("\n\tmaxsamp\t2_vs_3\twin_now\tmaxrisk\twinrisk\tcands\tcontest")
-        print("overall\t%.0f\t%.0f\t%.0f\t%.3f\t%.3f\t%s\t%s" % (max_sample_size, sample_size, sample_size_maj, max_risk, majority_risk, len(contest['choices']), contest['name']))
-        # print("2nd vs 3rd: %d mean sample size, %.3f max risk for contest with %d candidates: %s" % (sample_size, max_risk, len(contest['choices']), contest['name']))
-        # print("Majority: %d mean sample size, %.3f risk for contest with %d candidates: %s" % (sample_size_maj, majority_risk, len(contest['choices']), contest['name']))
     # Report minumum across all contests, then levels for all contests
 
 
