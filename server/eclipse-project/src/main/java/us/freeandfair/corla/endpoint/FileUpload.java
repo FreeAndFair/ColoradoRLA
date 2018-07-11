e82757dd37d777e364005fea8e42a7cd81cffd1b
Gordon Stratton <gordon@democracy.works>
2018-07-11 22:49:14 +0000

    Refactor CDOS code to appease static analysis

--- server/eclipse-project/src/main/java/us/freeandfair/corla/endpoint/FileUpload.java
+++ server/eclipse-project/src/main/java/us/freeandfair/corla/endpoint/FileUpload.java
@@ -15,12 +15,13 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.LineNumberReader;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -175,7 +176,6 @@ public class FileUpload extends AbstractEndpoint {
                             final Response the_response,
                             final UploadInformation the_info) {
     try {
-           
       final HttpServletRequest raw = SparkHelper.getRaw(the_request);
       the_info.my_ok = ServletFileUpload.isMultipartContent(raw);
 
@@ -187,8 +187,7 @@ public class FileUpload extends AbstractEndpoint {
           final FileItemStream item = fii.next();
           final String name = item.getFieldName();
           final InputStream stream = item.openStream();
-          final InputStream stream2 = item.openStream();
-          
+
           if (item.isFormField()) {
             the_info.my_form_fields.put(item.getFieldName(), Streams.asString(stream));
           } else if (FILE.equals(name)) {
@@ -228,118 +227,148 @@ public class FileUpload extends AbstractEndpoint {
     }
   }
 
-  
   /**
-   * Copies uploaded file which is in a temporary location and it's hash into an archival location
-   * 
+   * Copies uploaded file which is in a temporary location and its hash into
+   * an archival location
+   *
    * Steps:
    *  1. Based on operating system, fetches archival file path from property file
    *  2. Creates the file path if not existing
-   *  3. Appends timestamp to file and archives it by making a copy of the temporary file.
-   *  4. Appends the same timestamp to create a new file that would contain the hash value and archives it.  
-   *  
-   * @param uploadInformation contains all the specifices of the uploaded file that is in temporary location along with its hash value.
+   *  3. Appends timestamp to file and archives it by making a copy of the
+   *     temporary file.
+   *  4. Appends the same timestamp to create a new file that would contain the
+   *     hash value and archives it.
+   *
+   * @param the_upload_information contains all the specifices of the uploaded
+   *                               file that is in temporary location along with
+   *                               its hash value.
    */
-  private void archive(UploadInformation uploadInformation) {
-    
-      // name of file that was uploaded  
-      String uploadedFileName = uploadInformation.my_filename; 
-      
-      // Prepend timestamp to file name.
-      // Not append timestamp as we can't assure if file names follow filename.ext format) 
-      // Example: 2018-05-17-T10-44-04.244-Arapahoe 2016 Primary Ballot Manifest.csv
-      // Cannot use ':' as in HH:mm:ss because, when running in Windows, it throws java.nio.file.InvalidPathException
-      String arvchiveFileName = new SimpleDateFormat("yyyy-MM-dd-'T'HH-mm-ss.SSS-").format(new Date()) + uploadedFileName;
-      
-      // create corresponding hash file name to archive. Prepend archiveFileName so it is paired correctly 
-      String archiveHashFileName = null;      
-      if (StringUtils.contains(uploadedFileName, ".")) { // file name contains a "."
-         // get the file name till the "." and append "-Hash.text" to it
-         // Example: 
-         //        Archive File Name:      2018-05-17-T10-44-04.390-Arapahoe 2016 Primary Ballot Manifest.csv
-         //        Archive Hash File Name: 2018-05-17-T10-44-04.390-Arapahoe 2016 Primary Ballot Manifest-Hash.txt        
-         archiveHashFileName = StringUtils.substringBeforeLast(arvchiveFileName, ".") + "-Hash.txt";
-      } else {
-        // file name has no "." just append "-Hash.text" to it
-        // Example: 
-        //        Archive File Name:      2018-05-17-T10-44-04.390-Arapahoe 2016 Primary Ballot Manifest
-        //        Archive Hash File Name: 2018-05-17-T10-44-04.390-Arapahoe 2016 Primary Ballot Manifest-Hash.txt        
-        archiveHashFileName = arvchiveFileName + "-Hash.txt";
-      }
-  
-      // fetch location where file needs to be uploaded to for archival
-      String archiveFilePath = fetchArchiveFilePath();    
-      // create directory if not existing
-      File archiveFileDir = new File(archiveFilePath);      
-      archiveFileDir.mkdirs();    
-      
-      // archive file by copying it to destination
-      archiveFile (archiveFilePath+arvchiveFileName, uploadInformation.my_file.toPath());
-      // create corresponding hash text file with hash value in it
-      archiveHashFile(archiveFilePath+archiveHashFileName, uploadInformation.my_uploaded_hash);      
+  // we are deliberately ignoring the return value of archive_file_dir.mkdirs()
+  @SuppressFBWarnings("RV_RETURN_VALUE_IGNORED_BAD_PRACTICE")
+  private void archive(final UploadInformation the_upload_information) {
+    // name of file that was uploaded
+    final String uploaded_file_name = the_upload_information.my_filename;
 
+    // Prepend timestamp to file name.
+    // Not append timestamp as we can't assure if file names follow
+    // filename.ext format
+    // Example: 2018-05-17-T10-44-04.244-Arapahoe 2016 Primary Ballot Manifest.csv
+    // Cannot use ':' as in HH:mm:ss because, when running in Windows, it
+    // throws java.nio.file.InvalidPathException
+    final String archive_file_name =
+        new SimpleDateFormat("yyyy-MM-dd-'T'HH-mm-ss.SSS-",
+                             Locale.US).format(new Date()) +
+        uploaded_file_name;
+
+    // create corresponding hash file name to archive. Prepend archive_file_name
+    // so it is paired correctly
+    String archive_hash_file_name = null;
+    if (StringUtils.contains(uploaded_file_name, ".")) { // file name contains a "."
+      // get the file name till the "." and append "-Hash.text" to it
+      // Example:
+      //  Archive File Name:      2018-05-17-T10-44-04.390-Arapahoe 2016
+      //                          Primary Ballot Manifest.csv
+      //  Archive Hash File Name: 2018-05-17-T10-44-04.390-Arapahoe 2016
+      //                          Primary Ballot Manifest-Hash.txt
+      archive_hash_file_name = StringUtils.substringBeforeLast(archive_file_name, ".") +
+                               "-Hash.txt";
+    } else {
+      // file name has no "." just append "-Hash.text" to it
+      // Example:
+      //  Archive File Name:      2018-05-17-T10-44-04.390-Arapahoe 2016
+      //                          Primary Ballot Manifest
+      //  Archive Hash File Name: 2018-05-17-T10-44-04.390-Arapahoe 2016
+      //                          Primary Ballot Manifest-Hash.txt
+      archive_hash_file_name = archive_file_name + "-Hash.txt";
+    }
+
+    // fetch location where file needs to be uploaded to for archival
+    final String archive_file_path = fetchArchiveFilePath();
+    // create directory if not existing
+    final File archive_file_dir = new File(archive_file_path);
+    archive_file_dir.mkdirs();
+
+    // archive file by copying it to destination
+    archiveFile(archive_file_path + archive_file_name,
+                the_upload_information.my_file.toPath());
+    // create corresponding hash text file with hash value in it
+    archiveHashFile(archive_file_path + archive_hash_file_name,
+                    the_upload_information.my_uploaded_hash);
   }
 
   /**
-   * Copies passed in temporary file into archive destination, also renames it to its original name
-   * 
-   * @param filePathAndName path and file name of the  file to be archived
-   * @param sourcePath path and file name of the temporary file created by the server
+   * Copies passed in temporary file into archive destination, also renames it
+   * to its original name.
+   *
+   * @param the_file_path_and_name path and file name of the file to be archived
+   * @param the_source_path path and file name of the temporary file created by
+   *                        the server
    */
-  private void archiveFile(String filePathAndName, Path sourcePath)  {
-    try {        
-        // copy the temp file into archive destination
-        Path path = Files.copy( sourcePath, Paths.get(filePathAndName));        
-        
-        if (path != null) {
-           Main.LOGGER.info("Successfully archived file (" + filePathAndName + ")." );
-         } else {
-           Main.LOGGER.info("Error archiving file (" + filePathAndName + ")." );           
-         }
-                   
-      } catch (IOException e) {
-          Main.LOGGER.info("Encountered exception while archiving file (" + filePathAndName  + ") - " + e.getMessage());
+  private void archiveFile(final String the_file_path_and_name,
+                           final Path the_source_path) {
+    try {
+      // copy the temp file into archive destination
+      final Path path = Files.copy(the_source_path,
+                                   Paths.get(the_file_path_and_name));
+
+      if (path == null) {
+        Main.LOGGER.info("Error archiving file (" + the_file_path_and_name + ").");
+      } else {
+        Main.LOGGER.info("Successfully archived file (" + the_file_path_and_name + ").");
       }
+    } catch (final IOException e) {
+      Main.LOGGER.info("Encountered exception while archiving file (" +
+                       the_file_path_and_name +
+                       ")",
+                       e);
+    }
   }
-  
 
   /**
    * Creates a new hash file and copies passed in hash value and archives it.
-   * 
-   * @param archiveHashFileName path and file name of the hash file to be archived
-   * @param hashValue hash content that will be written into the file
+   *
+   * @param the_archive_hash_file_name path and file name of the hash file to be
+   *                                   archived
+   * @param the_hash_value hash content that will be written into the file
    */
-  private void archiveHashFile(String archiveHashFileName, String hashValue) {
-    try (BufferedWriter bw = new BufferedWriter(new FileWriter(archiveHashFileName))) {
+  private void archiveHashFile(final String the_archive_hash_file_name,
+                               final String the_hash_value) {
+    try (BufferedWriter bw =
+           new BufferedWriter(
+             new OutputStreamWriter(
+               new FileOutputStream(the_archive_hash_file_name),
+               StandardCharsets.UTF_8))) {
 
-      bw.write(hashValue);
-      Main.LOGGER.info("Successfully archived hash file (" + archiveHashFileName + ")." );
-
-    } catch (IOException e) {
-      Main.LOGGER.info("Encountered exception while archiving hash file (" + archiveHashFileName  + ") - " + e.getMessage());
-    }   
-    
+      bw.write(the_hash_value);
+      Main.LOGGER.info("Successfully archived hash file (" +
+                       the_archive_hash_file_name +
+                       ").");
+    } catch (final IOException e) {
+      Main.LOGGER.info("Encountered exception while archiving hash file (" +
+                       the_archive_hash_file_name +
+                       ")",
+                       e);
+    }
   }
-
 
   /**
    * Based on operating system, retrieves archive file location from property file.
-   * 
-   * @return archive file location 
+   *
+   * @return archive file location
    */
   private String fetchArchiveFilePath() {
-      Properties properties = Main.properties();
-      String osName = System.getProperty("os.name").toLowerCase();
-      boolean isWindows = osName.startsWith("windows");
-      String archiveFileLocation;
-      if (isWindows){
-        archiveFileLocation = properties.getProperty("windows_upload_file_location");
-      } else { // it's UNIX
-        archiveFileLocation = properties.getProperty("unix_upload_file_location");        
-      }
-      return archiveFileLocation;
+    final Properties properties = Main.properties();
+    final String os_name = System.getProperty("os.name").toLowerCase(Locale.US);
+    final boolean is_windows = os_name.startsWith("windows");
+    String archive_file_location;
+    if (is_windows) {
+      archive_file_location = properties.getProperty("windows_upload_file_location");
+    } else { // it's UNIX
+      archive_file_location = properties.getProperty("unix_upload_file_location");
+    }
+    return archive_file_location;
   }
-  
+
   /**
    * {@inheritDoc}
    * 
@@ -370,7 +399,7 @@ public class FileUpload extends AbstractEndpoint {
     
       if (info.my_ok) {
         info.my_computed_hash = HashChecker.hashFile(info.my_file);
-        info.my_uploaded_hash = 
+        info.my_uploaded_hash =
             info.my_form_fields.get(HASH).toUpperCase(Locale.US).trim();
         uploaded_file = attemptFilePersistence(the_response, info, county);
       }
@@ -383,7 +412,7 @@ public class FileUpload extends AbstractEndpoint {
       if (info.my_file != null) {
         try {
           // archive file before deleting
-          archive (info);
+          archive(info);
           if (!info.my_file.delete()) {
             Main.LOGGER.error("Unable to delete temp file " + info.my_file);
           }
