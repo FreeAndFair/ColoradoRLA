f5b153aac8822d5461e575a258d3b23bbd50a834
Neal McBurnett <nealmcb@freeandfair.us>
2018-09-25 10:07:04 -0600

    e-state-county test files: 4 contests, 3 counties
    See README.md for details
    Run audit.bash to use them
    
    Currently set up to avoid a bug with a contest tied in just one
    of its three counties.

--- test/e-state-county/README.md
+++ test/e-state-county/README.md
@@ -6,14 +6,16 @@ Run via `audit.bash`.
 
 Contests:
 
- * Prop 1, 20%, counties 1 2 and 3
- * Prop 2, 40%, counties 2 and 3
- * Prop 3, 10%, county 2
+ * Prop 1, 2%, counties 1 2 and 3 - opportunistic audit
+ * Prop 2, 20%, counties 1 2 and 3
+ * Prop 3, 40%, counties 2 and 3
+ * Prop 4, 10%, county 2
 
 Ballot counts per county: 1000, 10, 100
 
 CVR files generated via:
 
-    genelect 1000 200 > cvr-1000--200.csv
-    genelect 10 -2 4 1 > cvr-10--2-4-1.csv
-    genelect 100 24 40 > cvr-100--24-40.csv
+    genelect 1000 20 200 > cvr-1000--20-200.csv
+    genelect 10 0 -2 4 1 > cvr-10--0--2-4-1.csv
+    # alternative which doesn't trigger 'tie' bug: genelect 10 1 -2 4 1 > cvr-10--1--2-4-1.csv
+    genelect 100 2 24 40 > cvr-100--2-24-40.csv
--- test/e-state-county/audit.bash
+++ test/e-state-county/audit.bash
@@ -16,13 +16,15 @@ mkdir -p $LOGDIR
     echo -e "\nStart 'Define audit' step and upload county audit data\n"
     ./main.py dos_init
 
-    ./main.py -c 1 -f ../e-state-county/cvr-1000--200.csv -F ../e-state/manifest-1-1000.csv county_setup
-    ./main.py -c 2 -f ../e-state-county/cvr-10--2-4-1.csv -F ../e-state/manifest-2-10.csv county_setup
-    ./main.py -c 3 -f ../e-state-county/cvr-100--24-40.csv -F ../e-state/manifest-3-100.csv county_setup
+    ./main.py -c 1 -f ../e-state-county/cvr-1000--20-200.csv -F ../e-state/manifest-1-1000.csv county_setup
+    # Avoid bug with contest "Prop 1" tied in just one county, use alternate CVR with margin of 1 there
+    #./main.py -c 2 -f ../e-state-county/cvr-10--0--2-4-1.csv -F ../e-state/manifest-2-10.csv county_setup
+    ./main.py -c 2 -f ../e-state-county/cvr-10--1--2-4-1.csv -F ../e-state/manifest-2-10.csv county_setup
+    ./main.py -c 3 -f ../e-state-county/cvr-100--2-24-40.csv -F ../e-state/manifest-3-100.csv county_setup
 
     echo -e "\nFinish 'Define audit' step to select contests, enter the random seed, and Lanuch the Audit\n"
 
-    ./main.py -C 0 -C 1 -C 2 -C 3 -C 4 -C 5 dos_start
+    ./main.py -C 1  -C 3 -C 4 -C 5  -C 7 -C 8 dos_start
 
 ) | tee $LOGDIR/$LOGTAG.sm
 
